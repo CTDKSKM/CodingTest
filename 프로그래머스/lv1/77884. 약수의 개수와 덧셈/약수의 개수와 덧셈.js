5279d0cc2b788d34d8dd88730ce757984903bce5
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-17 13:00:47 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+    var answer = 0;
+    for(let i=left; i<=right; i++) {
+        let count = 1;
+        for(let j=1; j<=i/2; j++) {
+            i%j==0 ? count ++ : false
+        }
+        count % 2 == 0 ? answer += i : answer -= i
+    }
+    
+    return answer;
+}
