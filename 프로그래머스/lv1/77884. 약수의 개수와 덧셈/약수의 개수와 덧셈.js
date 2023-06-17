8c5e417557ef78f836e6a0c2738f935072a542b8
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-17 13:03:07 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,11 +1,14 @@
 function solution(left, right) {
-    var answer = 0;
-    for (let i = left; i <= right; i++) {
-        if (Number.isInteger(Math.sqrt(i))) {
-            answer -= i;
-        } else {
-            answer += i;
-        }
+    let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
     }
-    return answer
+    if (count % 2) answer -= i;
+    else answer += i;
+  }
+
+  return answer;
 }
