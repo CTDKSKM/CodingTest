4a48f57acd9fe5f31b37485b518e9cbda31ad544
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-19 13:42:56 +0900

    [unrated] Title: 세 개의 구분자, Time: 49.40 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,4 @@
 function solution(myStr) {
-    var answer = myStr.split("a").join(' ').split("b").join(' ').split("c").join(' ').split(' ').filter(v=>v);
+    var answer = myStr.replaceAll("a"," ").replaceAll("b"," ").replaceAll("c"," ").split(' ').filter(v=>v);
     return answer.length ? answer : ["EMPTY"];
 }
