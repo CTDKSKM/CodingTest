a97c600c5d944aedb3d778213e856db79243d706
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-20 10:26:42 +0900

    [level 0] Title: 공 던지기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,3 +1,22 @@
 function solution(numbers, k) {
-    return numbers[(--k*2)%numbers.length];
+    var answer = 0;
+    let oddNums = numbers.filter((num,idx)=>idx%2==0)
+    let evenNums = numbers.filter((num,idx)=>idx%2==1)
+    let newK = k%numbers.length
+    if (numbers.length%2 == 0) {
+        if (newK==0) {
+            answer = oddNums[oddNums.length-1]
+        } else {
+            newK > oddNums.length ? answer = oddNums[newK%oddNums.length-1] : answer = oddNums[newK-1]
+        }
+    } else {
+        if (newK > oddNums.length) {
+            answer = evenNums[newK%oddNums.length-1]
+        } else if (newK == 0) {
+            answer = evenNums[evenNums.length-1]
+        } else {
+            answer = oddNums[newK-1]
+        }
+    }
+    return answer;
 }
