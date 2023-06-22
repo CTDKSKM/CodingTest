3968b99e5cf5dff11cfdff12b38a2359c0d1dc78
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-22 19:12:00 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    var answer = true;
     if (s.length == 6 || s.length == 4) {
         !isNaN([...s].reduce((a,c)=>a-c)) ? answer = true : answer = false
     } else answer = false
