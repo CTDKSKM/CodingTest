fcf436ff80cdf7b8511d4a8cff99340127f6667f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-06-24 19:47:42 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,3 +1,12 @@
 function solution(numbers) {
-    return +numbers.replaceAll("zero", 0).replaceAll("one", 1).replaceAll("two", 2).replaceAll("three", 3).replaceAll("four", 4).replaceAll("five", 5).replaceAll("six",6).replaceAll("seven",7).replaceAll("eight",8).replaceAll("nine",9);
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+
+    return Number(num);
 }
