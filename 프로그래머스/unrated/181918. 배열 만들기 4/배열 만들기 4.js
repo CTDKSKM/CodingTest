fb5d1575c9d93b536ec2f6981075056a39dea58f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-05 22:16:56 +0900

    [unrated] Title: 배열 만들기 4, Time: 3.75 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,19 +1,14 @@
 function solution(arr) {
-    const stk = [];
-    // let i = 0
-    // for (let i = 0; i < arr.length; i += 1) {
-    //     while ((stk[stk.length-1] || 0) >= arr[i]) {
-    //         stk.pop()
-    //     }
-    //     stk.push(arr[i])
-    // }
-    let prev = 100001
-    for (let i = arr.length - 1; i >= 0; i -= 1) {
-        const v = arr[i]
-        if (v < prev) {
-            stk.unshift(v)
-            prev = v
+    var stk = [];
+    let i = 0 ; 
+    while( i < arr.length){
+        if(stk.length ===0) stk.push(arr[i++]);
+        else {
+            let val = stk[stk.length-1];
+            if( val < arr[i] ) stk.push(arr[i++]);
+            else stk.pop();
         }
+
     }
     return stk;
 }
