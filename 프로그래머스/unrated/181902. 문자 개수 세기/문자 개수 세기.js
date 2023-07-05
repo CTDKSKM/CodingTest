06f675ba4e71aa2766cc9b64e361db1789af818c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-05 19:47:04 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,8 +1,8 @@
 function solution(my_string) {
-    var answer = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     const arr = Array(52).fill(0)
     for(let i=0; i<my_string.length; i++) {
-        arr[answer.indexOf(my_string[i])]++
+        arr[alp.indexOf(my_string[i])]++
     }
 
     return arr;
