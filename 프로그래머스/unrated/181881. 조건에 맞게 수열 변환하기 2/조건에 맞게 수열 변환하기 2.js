fe5428a1b3bde3b464d4b79db1934239426f032f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-05 14:59:45 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,19 +1,18 @@
 function solution(arr) {
     var answer = 0;
-    for (let i =0; i>=0; i++) {
-        const tempArr = arr
-        arr = arr.map(item=>{
-            if (item >= 50) {
-                if (item % 2 == 0) return item/2
-                return item
-            } else if (item % 2 != 0) {
-                return item*2+1
-            }
-            return item
-        }) 
-        if (tempArr.reduce((acc,cur)=>acc+cur) === arr.reduce((acc,cur)=>acc+cur)) break
-        answer++
+    let before = [-1];
 
+    while(!arr.every((e,idx) => e == before[idx])) {
+        before =[...arr];
+
+        arr = arr.map(e => {
+            if(e >= 50 & e % 2 == 0)
+                return e / 2;
+            if(e < 50 & e % 2 != 0)
+                return e * 2 + 1;
+            return e;
+        })
+        answer++;
     }
-    return answer;
+    return answer - 1;
 }
