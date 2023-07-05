c34e3da6e68a09deebf6ab8dce018937b5cadcfd
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-05 15:00:57 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,11 +1,21 @@
 function solution(arr, n=0) {
-    while(!arr.every(x => (x >= 50 && x % 2 === 1) || (x < 50 && x % 2 === 0))) {
-        arr = arr.map(x => {
-            if (x >= 50 && x % 2 === 0) return x / 2;
-            if (x < 50 && x % 2 === 1) return x * 2 + 1;
-            return x;
-        })
-        n++;
-    }
-    return n;
+    let loopCount = 0;
+  const fibo = list => {
+    let count = 0;
+    const result = list.map(int => {
+      if (int >= 50 && int % 2 === 0) {
+        return int / 2;
+      } else if (int < 50 && int % 2 === 1) {
+        return int * 2 + 1;
+      }
+      count = count + 1;
+      return int;
+    });
+    if (count === list.length) return loopCount;
+    fibo(result);
+    loopCount = loopCount + 1;
+    console.log(result);
+    return loopCount;
+  };
+  return fibo(arr, arr.length);
 }
