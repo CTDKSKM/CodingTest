347298b349f540d558c47739a76f7738aeb808a6
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-07 12:57:31 +0900

    [level 0] Title: 안전지대, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,12 +1,69 @@
 function solution(board) {
-    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
-    let safezone = 0;
-
-    board.forEach((row, y, self) => row.forEach((it, x) => {
-        if (it === 1) return false;
-        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
-               ? false : safezone++;
-    }));
-
-    return safezone;
+    var answer = 0;
+    if (board.length==1) {
+        return board.flat().filter(v=>!v).length
+    }
+    for(let i=0; i<board.length; i++) {
+        for(let j=0; j<board.length; j++) {
+            if (board[i][j] == 1) {
+                if (i==0) {
+                    if (j==0) {
+                        !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                        !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                        !board[i+1][j+1] ? board[i+1][j+1] = 2 : board[i+1][j+1];
+                    } else if (j==board.length-1) {
+                        !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                        !board[i+1][j-1] ? board[i+1][j-1] = 2 : board[i+1][j-1];
+                        !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                    } else {
+                        !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                        !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                        !board[i+1][j-1] ? board[i+1][j-1] = 2 : board[i+1][j-1];
+                        !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                        !board[i+1][j+1] ? board[i+1][j+1] = 2 : board[i+1][j+1];
+                    }
+                } else if (j==0) {
+                    if (i==board.length-1) {
+                        !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                        !board[i-1][j+1] ? board[i-1][j+1] = 2 : board[i-1][j+1];
+                        !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                    } else {
+                        !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                        !board[i-1][j+1] ? board[i-1][j+1] = 2 : board[i-1][j+1];
+                        !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                        !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                        !board[i+1][j+1] ? board[i+1][j+1] = 2 : board[i+1][j+1];
+                    }
+                } else if (i==board.length-1) {
+                    if (j==board.length-1) {
+                        !board[i-1][j-1] ? board[i-1][j-1] = 2 : board[i-1][j-1];
+                        !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                        !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                    } else {
+                        !board[i-1][j-1] ? board[i-1][j-1] = 2 : board[i-1][j-1];
+                        !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                        !board[i-1][j+1] ? board[i-1][j+1] = 2 : board[i-1][j+1];
+                        !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                        !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                    }
+                } else if (j==board.length-1) {
+                    !board[i-1][j-1] ? board[i-1][j-1] = 2 : board[i-1][j-1];
+                    !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                    !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                    !board[i+1][j-1] ? board[i+1][j-1] = 2 : board[i+1][j-1];
+                    !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                } else {
+                    !board[i-1][j-1] ? board[i-1][j-1] = 2 : board[i-1][j-1];
+                    !board[i-1][j] ? board[i-1][j] = 2 : board[i-1][j];
+                    !board[i-1][j+1] ? board[i-1][j+1] = 2 : board[i-1][j+1];
+                    !board[i][j-1] ? board[i][j-1] = 2 : board[i][j-1];
+                    !board[i][j+1] ? board[i][j+1] = 2 : board[i][j+1];
+                    !board[i+1][j-1] ? board[i+1][j-1] = 2 : board[i+1][j-1];
+                    !board[i+1][j] ? board[i+1][j] = 2 : board[i+1][j];
+                    !board[i+1][j+1] ? board[i+1][j+1] = 2 : board[i+1][j+1];
+                }
+            }
+        }
+    }
+    return board.flat().filter(v=>!v).length;
 }
