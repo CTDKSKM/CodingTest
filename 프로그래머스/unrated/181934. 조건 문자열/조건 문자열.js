734d704f1f77d3b7439a2bdfc2be674ab07baaee
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-10 21:45:39 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,11 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if (ineq === ">") {
+        if (eq === "=") answer = n >= m
+        else answer = n>m
+    } else {
+        if (eq === "=") answer = n <= m
+        else answer = n<m
+    }
+    return answer ? 1 : 0;
+}
