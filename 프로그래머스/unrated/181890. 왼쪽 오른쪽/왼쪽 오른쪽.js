edac70d88d1892fb0bbef63e731c4acb246c4602
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-12 17:52:12 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,17 +1,8 @@
 function solution(str_list) {
-    const answer = []
     for(let i=0; i<str_list.length; i++){
-        if (str_list[i]==='l') {
-            if (i===0) break
-            answer.push(...str_list.slice(0,i))
-            break
-        }
-        if (str_list[i]==='r') {
-            if (i===str_list.length-1) break
-            answer.push(...str_list.slice(i+1))
-            break
-        }
+        if (str_list[i]==='l') return str_list.slice(0,i)
+        if (str_list[i]==='r') return str_list.slice(i+1)
     }
-    return answer
+    return []
 }
 
