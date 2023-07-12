0dbb35278dd641ecfc4ad97611fcb80affd0a2f2
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-12 18:26:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,9 +1,9 @@
 function solution(A,B){
     var answer = 0;
-    const sortedA = A.sort((a,b)=>b-a)
-    const sortedB = B.sort((a,b)=>a-b)
+    A.sort((a,b)=>b-a)
+    B.sort((a,b)=>a-b)
     for(let i = 0; i<A.length; i++) {
-        answer += sortedA[i] * sortedB[i]
+        answer += A[i] * B[i]
     }
 
     return answer;
