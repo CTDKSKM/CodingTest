11075bf65daa0af0e5a47330fdfabdedd7ea315a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-13 19:28:32 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,11 +1,3 @@
-function solution(spell, dic) {
-    var answer = 0;
-    dic.map(str=>{
-        let temp = 0;
-        for(let i =0; i<spell.length; i++) {
-            str.includes(spell[i]) && temp++
-        }
-        temp === spell.length ? answer = 1 : false
-    });
-    return answer ? 1 : 2
+function solution(p, d) {
+    return d.some(s => p.sort().toString() == [...s].sort().toString()) ? 1 : 2;
 }
