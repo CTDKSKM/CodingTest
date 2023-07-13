587bb319c849ab6a4587f2060f2cdf59f6c7c24f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-13 18:00:19 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 4.57 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,13 @@
+function solution(arr, queries) {
+    var answer = [];
+    queries.map(([s, e, k])=>{
+        let temp = [];
+        for(let i = s; i <= e; i++) {
+            if (arr[i] > k) {
+                temp.push(arr[i])
+            }
+        }
+        temp.length ? answer.push(Math.min(...temp)) : answer.push(-1)
+    })
+    return answer;
+}
