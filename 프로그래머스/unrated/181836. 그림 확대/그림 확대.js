2c66caacfd0af03fcf53775a627fdca738e89313
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-17 21:15:19 +0900

    [unrated] Title: 그림 확대, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -1,19 +1,11 @@
 function solution(picture, k) {
-    picture = picture.map((str, idx)=>{
-        let tempStr = ''
-        for(w of str) {
-            tempStr += w.repeat(k)
-        }
-        return tempStr
-    })
-    const answer = new Array(picture.length*k).fill(0).map((v, idx) =>
-        idx % k == 0 ? v=picture.shift() : 0
-    );
-    
-    return answer.map((str,idx)=>{
-        if (idx%k != 0) {
-            const temp = idx%k
-            return answer[idx-temp]
-        } else return str
-    })
+    var answer = [];
+
+    picture.forEach((line) => {
+        const expanded = [...line].reduce((acc, cur) => acc + cur.repeat(k), '');
+
+        for(let i=0; i<k; i++) answer.push(expanded);
+    });
+
+    return answer;
 }
