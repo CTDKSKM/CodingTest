34a3d154a8bd9c94d4be32fa5e9cb66975df3084
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-17 20:23:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,17 +1,12 @@
 function solution(keyinput, board) {
-    var answer = [0, 0];
-    const [c, r] = board;
-    const maxC = (c-1) / 2
-    const maxR = (r-1) / 2
-    keyinput.map(dir=>{
-        if (dir == "left") answer[0]--
-        if (dir == "right") answer[0]++
-        if (dir == "up") answer[1]++
-        if (dir == "down") answer[1]--
-        if (answer[0] < -maxC) answer[0]++
-        if (answer[0] > maxC) answer[0]--
-        if (answer[1] < -maxR) answer[1]++
-        if (answer[1] > maxR) answer[1]--
-    })
-    return answer;
+    let res = [0,0];
+    for (let p of keyinput) {
+        switch(p){
+            case 'left': if (-res[0] < board[0]/2-1) res[0]--; break;
+            case 'right': if (res[0] < board[0]/2-1) res[0]++; break;
+            case 'up': if (res[1] < board[1]/2-1) res[1]++; break;
+            case 'down': if (-res[1] < board[1]/2-1) res[1]--; break;
+        }
+    }
+    return res;
 }
