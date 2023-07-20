03e5c928d04e3e5598084845d88a9f2f6d24cd6c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-20 14:51:03 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,13 @@
+function solution(num, total) {
+    var answer = [];
+    if (!(num%2)) {
+        for(let i=0; i<num; i++) {
+            answer.push(Math.ceil(total/num)-(num/2)+i)
+        }
+    } else {
+        for(let i=0; i<num; i++) {
+            answer.push(total/num-(num-1)/2+i)
+        }
+    }
+    return answer
+}
