d8d34afb49af58b42155946508c0a948aaf8e886
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-25 21:42:51 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,26 +1,7 @@
 function solution(n, arr1, arr2) {
-    const a1 = [];
-    const a2 = [];
-    for(let i = 0; i<n; i++) {
-        a1.push(toBinary(arr1[i],n))
-        a2.push(toBinary(arr2[i],n))
-    }
-    return a1.map((v)=>toStr(v, 0, 7, null)).map((v,idx)=>+v + +a2[idx] + '').map(v=>toStr(v, 7,' ', '#'))
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
 }
 
-function toBinary(num,n) {
-    let temp = '';
-    for(let i=n-1; i>=0; i--) {
-        num-(2**i) >= 0 ? temp+='1' : temp+='0'
-        num %= (2**i)
-    }
-    return temp
-}
-
-function toStr(value, p1, p2, p3) {
-    let temp = '';
-    for(let a of value) {
-        a == p1 ? temp+=p2 : temp+=p3 ?? a
-    }
-    return temp
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
