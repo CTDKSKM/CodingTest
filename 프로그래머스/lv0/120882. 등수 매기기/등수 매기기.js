e8774645925cc2b6ffd2e0b566f5c0421842505e
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-26 13:52:30 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,12 +1,7 @@
 function solution(score) {
-    return score.map(([e, m]) => {
-        const me = e + m
-        let temp = 1;
-        for(let i=0; i<score.length; i++) {
-            const [e2, m2] = score[i]
-            if (me < e2 + m2) temp++
-        }
-        return temp
-    });
+    return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
 }
-// 다른 i 들의 값이 자신보다 크면 ++ 
