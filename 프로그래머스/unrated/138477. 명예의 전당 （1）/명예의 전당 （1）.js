9ccd952353c452145aa873708d10ea8a288b3f1e
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-26 10:14:26 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,20 +1,9 @@
 function solution(k, score) {
     var answer = [];
-    const hof = []
-    for(let i=0; i<k && i<score.length; i++) {
-        hof.push(score[i])
-        hof.sort((a,b)=>a-b)
-        answer.push(hof[0])
-    }
-    for(let i=k; i<score.length; i++) {
-        for(let j=0; j<hof.length; j++) {
-            if (score[i] > hof[j]) {
-                hof[j] = score[i]
-                hof.sort((a,b)=>a-b)
-                break
-            }
-        }
-        answer.push(hof[0])
-    }
-    return answer;
+
+    return score.reduce((acc, cur) => {
+        answer.push(cur);
+        answer = answer.sort((a, b) => b - a).slice(0, k);
+        return [...acc, Math.min(...answer)];
+    }, []);
 }
