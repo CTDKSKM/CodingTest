d90a6eca17516171b26aa2662168b36672782306
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-27 09:10:35 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,3 +1,9 @@
 function solution(numlist, n) {
-    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
+    numlist.sort((a,b)=>{
+        let n1 = Math.abs(a-n),
+            n2 = Math.abs(b-n);
+
+        return n1 < n2 ? -1 : n1 === n2 ? a < b ? 1 : -1 : 1;
+    });
+    return numlist;
 }
