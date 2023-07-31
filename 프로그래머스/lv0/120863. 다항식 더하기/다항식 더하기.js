5de32d12bb550ad5b4ce5c0e710b1fdb0a5f1705
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-31 17:38:09 +0900

    [level 0] Title: 다항식 더하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,10 @@
+function solution(polynomial) {
+    if (!polynomial.includes('x')) {
+        return polynomial.split(' + ').reduce((acc,cur)=>+acc + +cur) + '';
+    }
+    let 일차항부 = polynomial.split(' + ').filter(v=>v.includes('x')).map(v=>v=='x' ? '1' : v.slice(0,v.length-1)).reduce((acc,cur)=>+acc + +cur) + 'x';
+    if (일차항부=="1x") 일차항부 = "x"
+    const 상수항부 = polynomial.split(' + ').filter(v=>!v.includes('x')).reduce((acc,cur)=>+acc + +cur,0);
+    
+    return 상수항부 ? 일차항부 + ' + ' + 상수항부 : 일차항부
+}
