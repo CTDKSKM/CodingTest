3ebb6ee56200bafe76e40b29e3fcc87a93b3ea70
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-31 20:29:09 +0900

    [unrated] Title: 배열 만들기 2, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,12 +1,15 @@
-function* gen50() {
-    let i = 1;
 
-    while(true) {
-        yield Number(Number(i).toString(2)) * 5;
-        i++;
-    }
-}
 function solution(l, r) {
-    const result = Array.from({length:r-l+1}, (_,i)=>i+l).filter(n=>!/[^05]/.test(n));
-    return result.length ? result : [-1];
+    const arr = Array.from(String(r)).fill(false)
+    arr[arr.length-1] = true
+    const result = []
+    while (!arr.every(v => v === false)) {
+        const num = Number(arr.map(v => v ? 5 : 0).join(''))
+        if (num >= l && num <= r) result.push(num)
+        for (let i = arr.length-1; i >= 0; i -= 1) {
+            arr[i] = !arr[i]
+            if (arr[i]) break
+        }
+    }
+    return result.length === 0 ? [-1] : result
 }
