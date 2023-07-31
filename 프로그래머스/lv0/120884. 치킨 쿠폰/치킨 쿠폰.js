2d8141601ebd3862cb66520e715be354e824289b
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-07-31 15:22:28 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,10 +1,11 @@
 function solution(chicken) {
-    var answer = 0;
+    let answer = 0;
     let coupon = chicken
-    for(let i = 0; coupon >= 10; i++) {
-        coupon -= 10
-        answer++
-        coupon++
+
+    while(coupon >= 10){
+        answer = answer + parseInt(coupon/10)
+        coupon = parseInt(coupon/10)+ coupon%10
     }
+
     return answer;
 }
