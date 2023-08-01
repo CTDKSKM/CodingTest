ce10031ee3c24b50d86820901b0db078c07b6510
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-01 16:00:32 +0900

    [level 1] Title: 시저 암호, Time: 30.65 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,17 @@
+function solution(s, n) {
+    const upperAlp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerAlp = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+    const answer = [];
+    
+    for(let i=0; i<s.length; i++) {
+        if (s[i] != ' ') {
+            if (upperAlp.includes(s[i])) {
+                answer.push(upperAlp[upperAlp.indexOf(s[i])+n]);
+            } else {
+                answer.push(lowerAlp[lowerAlp.indexOf(s[i])+n]);
+            }
+        } else answer.push(' ')
+    }
+    
+    return answer.join('');
+}
