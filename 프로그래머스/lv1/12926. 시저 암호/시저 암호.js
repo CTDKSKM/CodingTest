69c03779ca4bd014df3575325c6dbef396e2cc46
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-01 16:07:16 +0900

    [level 1] Title: 시저 암호, Time: 7.49 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,4 +1,17 @@
 function solution(s, n) {
-    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
-    return s.split('').map(e => chars[chars.indexOf(e)+n]).join('');
+    const upperAlp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerAlp = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+    const answer = [];
+
+    for(let i=0; i<s.length; i++) {
+        if (s[i] != ' ') {
+            if (upperAlp.includes(s[i])) {
+                answer.push(upperAlp[upperAlp.indexOf(s[i])+n]);
+            } else {
+                answer.push(lowerAlp[lowerAlp.indexOf(s[i])+n]);
+            }
+        } else answer.push(' ')
+    }
+
+    return answer.join('');
 }
