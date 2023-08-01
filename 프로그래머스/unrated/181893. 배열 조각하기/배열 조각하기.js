df3f02ed36a242336a6c88d83e1fb821f442935d
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-01 13:50:37 +0900

    [unrated] Title: 배열 조각하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,7 +1,20 @@
 function solution(arr, query) {
-    for(let i=0; i<query.length; i++) {
-        i%2 ? arr.splice(0,query[i]) : arr.splice(query[i]+1)
-    }
+    let answer = [...arr];
+   var f=0;
+    var e=0;
 
-    return arr;
+  for(let i=0;i<query.length;i++){
+
+    if(i%2===0){//짝수
+        e=f+query[i];
+      //answer = answer.slice(0,query[i])
+    }else if(i%2===1){//홀수
+        f=f+query[i];
+      //answer = answer.slice(query[i],answer.length);
+    }
+  }
+
+  answer=answer.slice(f,e+1);
+
+  return answer.length>0?answer:[-1]
 }
