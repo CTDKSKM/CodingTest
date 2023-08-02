a5d21cd484d736110d794220fd5c7a05abc1bd2a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-03 00:19:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 45.28 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,9 +2,7 @@ function solution(s)
 {
     const arr = []
     for(let i=0; i<s.length; i++) {
-        if (arr.length) {
             arr[arr.length-1] == s[i] ? arr.pop() : arr.push(s[i])
-        } else arr.push(s[i])
     }
     return arr.length ? 0 : 1
 //     s = [...s]
