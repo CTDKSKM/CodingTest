72129a1e60920cbd790196de0ccae4779418953a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-03 11:31:21 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -2,10 +2,7 @@ function solution(food) {
     var answer = '';
     for(let i=1; i<food.length; i++) {
         const temp = i.toString()
-        if (food[i] == 1) continue
-        if (food[i] % 2) {
-            answer += temp.repeat((food[i]-1)/2)
-        } else answer += temp.repeat(food[i]/2)
+            answer += temp.repeat(Math.floor((food[i])/2))
     }
     return answer + '0' + [...answer].reverse().join('');
 }
