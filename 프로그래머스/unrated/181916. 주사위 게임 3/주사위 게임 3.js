6de9e8084e7f724136ae891c44a52c8404678538
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-03 23:40:50 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,14 +1,14 @@
 function solution(...args) {
     const dice = new Array(6).fill(0)
-    
+
     for(let i=0; i<args.length; i++) {
         dice[args[i]-1]++
     }
-    
+
     if (dice.includes(4)) return 1111*(dice.indexOf(4)+1)
-    
+
     if (dice.includes(3)) return (10*(dice.indexOf(3)+1)+(dice.indexOf(1)+1))**2
-    
+
     if (dice.filter(v=>v==2).length==2) return ((dice.indexOf(2)+1)+(dice.lastIndexOf(2)+1))*Math.abs((dice.indexOf(2)+1)-(dice.lastIndexOf(2)+1))
     else if (dice.filter(v=>v==2).length==1) return (dice.indexOf(1)+1)*(dice.lastIndexOf(1)+1)
     else return dice.indexOf(1)+1;
