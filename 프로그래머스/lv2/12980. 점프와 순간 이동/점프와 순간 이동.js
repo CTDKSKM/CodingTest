91275ba4b7106c93b6383d3de6083a369b3007b0
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-04 10:02:23 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,6 @@
 function solution(n)
 {
-    var ans = 0;
-    for(let i=n; i>0; i/=2) {
-        if (i%2==0) continue
-        i--
-        ans++
-    }
-    return ans;
+    if(n === 1) return 1;
+    const nArr = Array.from(n.toString(2));
+    return nArr.reduce((a,b)=>(+a)+(+b));
 }
-// 나눠지면 나누고 아니면 -1해서 건전지사용.
