5c48e51efdb611679e0f55385acf10628c80ac59
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-05 22:36:06 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,18 +1,30 @@
 function solution(n) {
-    let ans = Array.from({length:n},()=>[]);
+    let result = new Array(n);
+    for(let i = 0; i < result.length; i++){
+        result[i] = new Array(n);
+    }
+    let direction = 1; 
+    let x,y,num;
+    x = y = num = 0;
+    x--; 
 
-    let rowMin = 1, rowMax = n-1;
-    let colMin = 0, colMax = n-1;
-    let row = col = 0;
+    while(1) {
 
-    let dir = 'r';
-    for (let i = 1; i <= n*n; i++) {
-        ans[row][col]=i;
-        if (dir === 'r') if (col === colMax) colMax--,row++,dir = 'b'; else col++;
-        else if (dir === 'l') if (col === colMin) colMin++,row--,dir = 't'; else col--;
-        else if (dir === 'b') if (row === rowMax) rowMax--, col--, dir = 'l'; else row++;
-        else if (dir === 't') if (row === rowMin) rowMin++, col++, dir = 'r'; else row--;
+        for(let i = 0; i < n; i++){
+            x += direction;
+            result[y][x] = ++num; 
+        }
+
+        n--; 
+        if(n == 0)break;
+
+
+        for(let j = 0; j < n; j++){
+            y += direction;
+            result[y][x] = ++num;
+        }
+        direction *= -1;
     }
 
-    return ans;
+    return result;
 }
