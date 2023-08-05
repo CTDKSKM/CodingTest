b9f822c838fb9953c819e07c57b4ba542d0adfbe
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-05 23:16:45 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,10 +1,10 @@
 function solution(s) {
-    var answer = [];
-    s.slice(1,-1).split('{').join('').split('},').sort((a,b)=>a.length-b.length).map(v=>v.split(',')).map(v=>{
-        v.forEach(val=>{
-            if (val.includes('}')) val=val.slice(0,-1);
-            answer.includes(+val) ? false : answer.push(+val) 
-        })
-    })
-    return answer
+    return JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
+    .sort((a, b) => a.length - b.length)
+    .reduce((arr, v, n) => {
+        if (n) {
+            return arr.concat(v.filter(f => !arr.includes(f)));
+        }
+        return v;
+    }, []);
 }
