01aea48dd5b88317945e865f729fb6547e2d2214
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-06 22:22:22 +0900

    [level 1] Title: 모의고사, Time: 3.15 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,18 +1,19 @@
 function solution(answers) {
-    var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const arr = [0,0,0];
+    const sp1 = [1,2,3,4,5]
+    const sp2 = [2,1,2,3,2,4,2,5]
+    const sp3 = [3,3,1,1,2,2,4,4,5,5]
+    for(let i=0; i<answers.length; i++) {
+        if (sp1[i%5] == answers[i]) arr[0]++
+        if (sp2[i%8] == answers[i]) arr[1]++
+        if (sp3[i%10] == answers[i]) arr[2]++
+    }
+    
+    const ans = []
+    for(let i=0; i<arr.length; i++) {
+        if (arr[i] == Math.max(...arr)) ans.push(i+1)
+    }
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
-
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
-
-
-    return answer;
+    return ans
+    // return arr.map(v=>v == Math.max(...arr) ? v : -1).map((v,idx)=>v!=-1 ? idx+1 : v).filter(v=>v!=-1)
 }
