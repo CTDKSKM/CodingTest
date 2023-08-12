8fc9cd1d77015fee761b577346ea07e798498cbb
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-13 05:17:24 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,14 +1,15 @@
 function solution(lines) {
-    var answer = new Set()
-    lines.sort((a,b)=>a[1]-b[1])
-    for (let i=0; i<lines.length-1; i++) {
-        for (let j=i+1; j<lines.length; j++) {
-            const [bs, be] = lines[i]
-            const [cs, ce] = lines[j]
-            for(let k=cs > bs ? cs : bs; k<be && k<ce; k++) {
-                answer.add(k)
+    let dist = new Set();
+    for(let i = 0; i < lines.length; i++) {
+        for(let j = i+1; j < lines.length; j++) {
+            let [curX, curY] = lines[i];
+            let [nextX, nextY] = lines[j];
+            if(Math.min(curY, nextY) > Math.max(curX, nextX)) {
+                for(let k = Math.max(curX, nextX); k < Math.min(curY, nextY); k++) {
+                    dist.add(k)
+                }
             }
         }
     }
-    return answer.size;
+    return dist.size
 }
