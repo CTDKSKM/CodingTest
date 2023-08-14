17a8d6e1a4212232c39bd6f11c20cf935b92ca25
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-14 18:06:26 +0900

    [level 0] Title: 평행, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -0,0 +1,16 @@
+function solution(dots) {
+    var answer = 0;
+    const 기울기들 = {}
+    for(let i=0; i<dots.length; i++) {
+        for(let j=i+1; j<dots.length; j++) {
+            const [x1, y1] = dots[i]
+            const [x2, y2] = dots[j]
+            const 기울기 = (x2-x1) / (y2-y1)
+            기울기들[`${i}${j}`] = 기울기
+        }
+    }
+    if (기울기들["01"] === 기울기들["23"]) return 1
+    if (기울기들["02"] === 기울기들["13"]) return 1
+    if (기울기들["03"] === 기울기들["12"]) return 1
+    return 0;
+}
