57c817906402e46610a44742c44298041c884bdf
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-20 14:12:18 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,28 +1,19 @@
 function solution(s) {
-    if(s.length % 2 === 1) return 0;
-
     let answer = 0;
-    const mapping = { "}" : "{", "]" : "[", ")" : "("};
 
-    for(let i = 0; i < s.length; i++) {
-        const stack = [];
-        const rotate = s.slice(i) + s.slice(0, i);
-        let flag = true;
-
-        for(let j = 0; j < s.length; j++) {
-            if(rotate[j] === "[" || rotate[j] === "(" || rotate[j] === "{" )
-                stack.push(rotate[j]);
-            else {
-                const last = stack.pop();
-                if(last !== mapping[rotate[j]]) {
-                    flag = false
-                    break;
-                }
+    for(let i=0; i<s.length; i++) {
+        let str = s.slice(i) + s.slice(0, i)
+        while (true) {
+            const len = str.length
+            str = str.replaceAll('()', '')
+            str = str.replaceAll('{}', '')
+            str = str.replaceAll('[]', '')
+            if (str.length == 0) {
+                answer++
+                break
             }
+            if (len == str.length) break
         }
-
-        if(flag) answer++;
     }
-
     return answer;
 }
