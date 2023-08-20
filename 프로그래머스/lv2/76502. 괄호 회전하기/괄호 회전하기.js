9d1ad5b95150284ddf425bcaac488b4ba883321f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-20 14:08:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,36 +1,28 @@
 function solution(s) {
-    const len = s.length;
-  let cnt = 0;
-  if (s.length % 2 !== 0) return 0;
-  let arr = [...s];
-  let temp;
-  let istrue = true;
-  for (let i = 0; i < len; i++) {  
-    let pre = [];
-    for (let j = 0; j < len; j++) {
-      if (arr[j] === "(" || arr[j] === "{" || arr[j] === "[") {
-        pre.push(arr[j]);
-      } else {
-        const temp2 = pre.pop();
-        if (temp2 === undefined) {
-            istrue = false;
+    if(s.length % 2 === 1) return 0;
+
+    let answer = 0;
+    const mapping = { "}" : "{", "]" : "[", ")" : "("};
+
+    for(let i = 0; i < s.length; i++) {
+        const stack = [];
+        const rotate = s.slice(i) + s.slice(0, i);
+        let flag = true;
+
+        for(let j = 0; j < s.length; j++) {
+            if(rotate[j] === "[" || rotate[j] === "(" || rotate[j] === "{" )
+                stack.push(rotate[j]);
+            else {
+                const last = stack.pop();
+                if(last !== mapping[rotate[j]]) {
+                    flag = false
+                    break;
+                }
+            }
         }
-        else if (
-          arr[j].charCodeAt() !== temp2.charCodeAt() + 1 &&
-          arr[j].charCodeAt() !== temp2.charCodeAt() + 2
-        ) {
-          console.log(arr[j].charCodeAt(0), temp2 !== undefined && temp2.charCodeAt() + 1);
-          istrue = false;
-        }
-      }
-      if (istrue === false) break;
+
+        if(flag) answer++;
     }
 
-    if (istrue === true) ++cnt;
-    temp = arr.shift();
-    arr.push(temp);
-    istrue = true;
-  }
-
-  return cnt;
+    return answer;
 }
