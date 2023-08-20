716acf03ced88731def5707743acece28280a826
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-20 14:07:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 3.65 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,18 +1,36 @@
 function solution(s) {
-    let answer = 0;
-    for(let i=0; i<s.length; i++) {
-        let str = s.slice(i) + s.slice(0, i)
-        while (true) {
-            const len = str.length
-            str = str.replaceAll('()', '')
-            str = str.replaceAll('{}', '')
-            str = str.replaceAll('[]', '')
-            if (str.length == 0) {
-                answer++
-                break
-            }
-            if (len == str.length) break
+    const len = s.length;
+  let cnt = 0;
+  if (s.length % 2 !== 0) return 0;
+  let arr = [...s];
+  let temp;
+  let istrue = true;
+  for (let i = 0; i < len; i++) {  
+    let pre = [];
+    for (let j = 0; j < len; j++) {
+      if (arr[j] === "(" || arr[j] === "{" || arr[j] === "[") {
+        pre.push(arr[j]);
+      } else {
+        const temp2 = pre.pop();
+        if (temp2 === undefined) {
+            istrue = false;
         }
+        else if (
+          arr[j].charCodeAt() !== temp2.charCodeAt() + 1 &&
+          arr[j].charCodeAt() !== temp2.charCodeAt() + 2
+        ) {
+          console.log(arr[j].charCodeAt(0), temp2 !== undefined && temp2.charCodeAt() + 1);
+          istrue = false;
+        }
+      }
+      if (istrue === false) break;
     }
-    return answer;
+
+    if (istrue === true) ++cnt;
+    temp = arr.shift();
+    arr.push(temp);
+    istrue = true;
+  }
+
+  return cnt;
 }
