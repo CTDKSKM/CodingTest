82d0acab9cf0ace4845ccbe52765599b1620cfda
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-08-22 14:45:32 +0900

    [level 2] Title: 가장 큰 수, Time: 16.23 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,13 +1,12 @@
 function solution(numbers) {
-    const largest = numbers.sort((a, b) => {
-        const order1 = `${a}${b}`;
-        const order2 = `${b}${a}`;
-        return order2.localeCompare(order1);
-    }).join('');
+    const result = numbers.sort((a, b) => {
+        const strA = String(a);
+        const strB = String(b);
 
-    if (largest[0] === '0') {
-        return '0';
-    }
-
-    return largest;
+        return (strB + strA).localeCompare(strA + strB);
+    }).join('')
+    
+    if (result[0] === '0') return '0'
+    
+    return result
 }
