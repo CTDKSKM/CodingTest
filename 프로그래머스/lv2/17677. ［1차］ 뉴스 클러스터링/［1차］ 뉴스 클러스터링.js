5b27bb108e935a2e791a633c8be483ff780d71af
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-05 23:02:57 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,42 +1,59 @@
 function solution(str1, str2) {
-    const arr1 = generateSubsets(str1);
-    const arr2 = generateSubsets(str2);
+    // 물밑작업하자
+    const a = prep(str1), b = prep(str2);
 
-    return calcJaccardSimilarity(arr1, arr2);
-}
+    // 교, 합집합을 찾아보자
+    const int = {}, uni = {};
 
-function generateSubsets(str) {
-    const normalizedStr = str.toLowerCase();
-    const subsets = [];
-
-    for (let i = 0; i < normalizedStr.length - 1; i++) {
-        const subset = normalizedStr.slice(i, i + 2);
-        if (!/[a-z]{2}/.test(subset)) continue;
-        subsets.push(subset);
-    }
-
-    return subsets.sort();
-}
-
-function calcJaccardSimilarity(arr1, arr2) {
-    const union = new Map()
-    const intersection = new Map()
-
-    for (const element of arr1) {
-        union.set(element, Math.max(union.get(element) || 0, arr1.filter(e => e === element).length));
-    }
-    for (const element of arr2) {
-        union.set(element, Math.max(union.get(element) || 0, arr2.filter(e => e === element).length));
-    }
-    
-    for (const element of arr1) {
-        if (arr2.includes(element)) {
-            const minCount = Math.min(arr1.filter(e => e === element).length, arr2.filter(e => e === element).length);
-            intersection.set(element, minCount);
+    // a집합을 먼저 둘러보자
+    for (const p in a){
+        if(b[p] === undefined) {
+            // b 집합에는 없다! => 합집합에만 넣자
+            uni[p] = a[p];
+        } else {
+            // 양쪽에 다있다! => 양쪽에 다넣자
+            // 교집합의 경우 더 작은 수를, 합집합의 경우 더 큰 수를 넣자.
+            int[p] = Math.min(a[p], b[p]);
+            uni[p] = Math.max(a[p], b[p]);
         }
     }
-    const intersectionSize = [...intersection.values()].reduce((acc, val) => acc + val, 0);
-    const unionSize = [...union.values()].reduce((acc, val) => acc + val, 0);
+    // 이 단계에서 교집합은 완료 되었다
 
-    return unionSize === 0 ? 65536 : Math.floor(intersectionSize / unionSize * 65536);
+    // b 집합에만 있는 녀석들을 합집합에 추가해주자
+    for (const p in b){
+        if(uni[p] === undefined) {
+            uni[p] = b[p];
+        }
+    }
+
+    // 갯수를 셔보자
+    let iC = 0, uC = 0;
+
+    for (const p in int) iC += int[p];
+    for (const p in uni) uC += uni[p];
+
+    // 합집합이가 공집합이면 65536을, 아니면 시키는대로 해서
+    // 돌려주자 (~~ 는 양수에 적용 가능한 플로어링 테크닉임)
+
+    return uC ? ~~(65536*iC/uC) : 65536;
+}
+
+function prep(str){
+    // 대소문자 동일 취급
+    str = str.toLowerCase();
+
+    // {요소: 횟수}로 매핑해보자
+    const ans = {};
+    for (let i = 0; i < str.length-1; i++){
+        // 두 캐릭터 중에 하나라도 알파벳 아니면 무시하자
+        if (str[i] < 'a' || 'z' < str[i]) continue;
+        if (str[i+1] < 'a' || 'z' < str[i+1]) continue;
+
+        // 이 요소를 key로, 이 요소의 등장횟수를 value로 기록하자
+        const el = str[i]+str[i+1];
+        ans[el] = (ans[el]||0) + 1;
+    }
+
+    // 돌려주자
+    return ans;
 }
