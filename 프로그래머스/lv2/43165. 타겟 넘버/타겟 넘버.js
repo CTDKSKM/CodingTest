d001bc9e6571b73980f0ffc63cc18de479152b11
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-08 06:56:35 +0900

    [level 2] Title: 타겟 넘버, Time: 1.07 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,15 @@
 function solution(numbers, target) {
     let count = 0;
-
-    function dfs(index, currentSum) {
-        if (index === numbers.length) {
-            if (currentSum === target) {
-                count++;
-            }
-            return;
+    function dfs(i, sum) {
+        if (i == numbers.length) {
+            if (sum == target) count++
+            return
         }
-
-        dfs(index + 1, currentSum + numbers[index]);
-        dfs(index + 1, currentSum - numbers[index]);
+        
+        dfs(i+1, sum + numbers[i])
+        dfs(i+1, sum - numbers[i])
     }
-    dfs(0, 0);
-    
-    return count
+    dfs(0, 0)
+
+    return count;
 }
