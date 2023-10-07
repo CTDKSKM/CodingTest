d25f106dd45625fefeefe7a06131b2b99b017b6a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-07 17:55:54 +0900

    [level 2] Title: 피로도, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,1 +1,37 @@
-let solution = (k, d) => Math.max(...d.map(([m, v], i) => k < m ? 0 : solution(k - v, [...d.slice(0, i), ...d.slice(i + 1)]) + 1), 0)
+function solution(k, dungeons) {
+    let result = 0;
+    const tempArr = getPermutations(Array(dungeons.length).fill(0).map((_,idx)=>idx))
+    for(let i=0; i<tempArr.length; i++) {
+        let copyK = k
+        let temp = 0;
+        for(let j=0; j<tempArr[0].length; j++) {
+            if (copyK>=dungeons[tempArr[i][j]][0]) {
+                copyK -= dungeons[tempArr[i][j]][1]
+                temp++
+            }
+        }
+        temp > result ? result = temp : false
+    }
+    return result;
+}
+
+function getPermutations(arr) {
+  const permutations = [];
+
+  function generatePermutations(currentPermutation, remainingElements) {
+    if (remainingElements.length === 0) {
+      permutations.push(currentPermutation);
+      return;
+    }
+
+    for (let i = 0; i < remainingElements.length; i++) {
+      const updatedPermutation = currentPermutation.concat(remainingElements[i]);
+      const remainingElementsCopy = [...remainingElements];
+      remainingElementsCopy.splice(i, 1);
+      generatePermutations(updatedPermutation, remainingElementsCopy);
+    }
+  }
+
+  generatePermutations([], arr);
+  return permutations;
+}
