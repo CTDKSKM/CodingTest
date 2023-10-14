739e24bc72cb1831ac44c2ec606112df8e28faa8
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-15 07:26:16 +0900

    [level 2] Title: 더 맵게, Time: 625.98 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,105 +1,19 @@
-class MinHeap {
-    //생성자 함수
-    constructor() {
-        this.heap = [];
-    }
-
-    //index를 가져오는 함수
-    getParentIdx(childIdx) {
-        return Math.floor((childIdx - 1) / 2);
-    }
-    getLeftChildIdx(parentIdx) {
-        return (parentIdx *  2) + 1;
-    }
-    getRightChildIdx(parentIdx) {
-        return (parentIdx * 2) + 2;
-    }
-
-    //heap tree의 노드개수를 세어주는 함수
-    size() {
-        return this.heap.length;
-    }
-
-    //두 값을 swap해준다.
-    swap(Idx_1, Idx_2) {
-        [this.heap[Idx_1], this.heap[Idx_2]] = [this.heap[Idx_2], this.heap[Idx_1]];
-
-        return this.heap;
-    }
-
-    //새로운 노드를 heap tree의 마지막 노드에 push해주는 함수
-    push(value) {
-        this.heap.push(value);
-        this.bubbleUp();
-
-        return this.heap;
-    }
-    //최상단 head node를 pop해주는 함수
-    pop() {
-        if(this.size() == 1) {
-            return this.heap.pop();
-        }
-        if(this.size() == 0) {
-            return null;
-        }
-        const value = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.bubbleDown();
-        return value;
-    }
-
-    //맨 마지막 노드에서 출발해서 최상단 노드까지 올라가면서 bubble up 한다.
-    bubbleUp() {
-        let child_Idx = this.size() - 1;
-        let parent_Idx = this.getParentIdx(child_Idx);
-
-        while (this.heap[child_Idx] < this.heap[parent_Idx]) {
-            this.swap(child_Idx, parent_Idx);
-            child_Idx = parent_Idx;
-            parent_Idx = this.getParentIdx(child_Idx);
-        }
-    }
-
-    //최상단 노드에서 마지막노드까지 내려가면서 bubble down 한다.
-    bubbleDown() {
-        let parent_Idx = 0;
-        let left_Idx = this.getLeftChildIdx(parent_Idx);
-        let right_Idx = this.getRightChildIdx(parent_Idx);
-
-        while((left_Idx <= this.size() - 1 && this.heap[left_Idx] < this.heap[parent_Idx]) || (right_Idx <= this.size() - 1 && this.heap[right_Idx] < this.heap[parent_Idx])) {
-            //오른쪽이 왼쪽보다 작고 오른쪽 노드가 존재한다면
-            if(this.heap[right_Idx] < this.heap[left_Idx] && right_Idx <= this.size() - 1){
-                //오른쪽과 부모노드를 swap 한다.
-                this.swap(right_Idx, parent_Idx);
-                parent_Idx = right_Idx;
-                right_Idx = this.getRightChildIdx(parent_Idx);
-                left_Idx = this.getLeftChildIdx(parent_Idx);
-            } else {
-                //왼쪽과 부모노드를 swap 한다.
-                this.swap(left_Idx, parent_Idx);
-                parent_Idx = left_Idx;
-                right_Idx = this.getRightChildIdx(parent_Idx);
-                left_Idx = this.getLeftChildIdx(parent_Idx);
-            }
-        }
-    }
-}
-
-
 function solution(scoville, K) {
     scoville.sort((a,b)=> b - a) 
     let cnt = 0 ;
     let under_K = []
     let flag = 0 // 다 합쳐서 K보다 큰지 안큰지 판별 0이면 안큰거 1이면 큰거
 
-    for(let i = 0 ; i < scoville.length ; ++i){
+    for(let i = scoville.length-1 ; i>=0 ; i--){
         if(scoville[i] < K){
             under_K.push(scoville[i])
         }
         else{
             flag = 1
+            break
         }
     }    
+    under_K.sort((a,b)=> b - a)
    // console.log(under_K)
     let mixed = []
     let m_i = 0
