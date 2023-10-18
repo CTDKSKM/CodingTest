4931a5140554f22183d2f82a3aa3d9a6e0b5662c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-18 19:15:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 29.27 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,1 +1,17 @@
-const solution = (participant, completion) => participant.find(name=>!completion[name]--,completion.map(name=>completion[name]=(completion[name]|0)+1))
+const solution = (participant, completion) => {
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
+}
