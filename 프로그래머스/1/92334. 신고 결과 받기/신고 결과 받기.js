da5ac668b93671aa24cb565abf11e2e99b3b62b0
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-20 11:10:47 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.16 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,31 +1,17 @@
 function solution(id_list, report, k) {
-    const users = new Map();
-    const reporters = new Map();
-    for (const user of id_list) users.set(user, 0)
-    
-    for (const val of report) {
-        const [user, reporter] = val.split(' ')
-        
-        if (reporters.has(reporter)) {
-            if (!reporters.get(reporter).includes(user)) {
-                reporters.set(reporter, [...reporters.get(reporter),user])
-            }
-        }
-        else {
-            reporters.set(reporter, [user])
+    let reports = [...new Set(report)].map(a=>{return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports){
+        counts.set(bad[1],counts.get(bad[1])+1||1)
+    }
+    let good = new Map();
+    for(const report of reports){
+        if(counts.get(report[1])>=k){
+            good.set(report[0],good.get(report[0])+1||1)
         }
     }
-    
-    for (const val of reporters) {
-        const [reporter, uids] = val
-        if (uids.length >= k) {
-            for(const user of uids) {
-                users.set(user, users.get(user) + 1)
-            }
-        }
-    }
-    
-    return Array(...users).map(val=>val[1])
+    let answer = id_list.map(a=>good.get(a)||0)
+    return answer;
 }
 
 //피신고자에 신고자 매핑. size >= k 일 시 신고자들 cnt++ 
