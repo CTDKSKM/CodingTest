cbb6455ad5bc6d3f7325b2ea4607a92e81662ceb
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-22 13:31:19 +0900

    [level 3] Title: 단어 변환, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,30 +1,37 @@
 function solution(begin, target, words) {
-    const seen = []
-    const queue = [[begin, 0]]
-    let flag = 0;
-    let ans;
-    while(queue.length) {
-        const [now,count] = queue.shift()
+    const seen = new Set();
+    const queue = [[begin, 0]];
+    let found = false;
+    let result;
 
-        if (now == target) {
-            flag = 1;
-            ans = count
-            break
+    while (queue.length) {
+        const [currentWord, count] = queue.shift();
+
+        if (currentWord === target) {
+            found = true;
+            result = count;
+            break;
         }
-        for(let i=0; i<words.length; i++) {
-            let temp = 0;
 
-            for(let j=0; j<target.length; j++) {
-                now[j] == words[i][j] ? temp++ : false
-            }
-            if (temp == begin.length-1) {
-                if (!seen.includes(words[i])) {
-                    seen.push(words[i])
-                    queue.push([words[i],count + 1])
+        for (let i = 0; i < words.length; i++) {
+            if (!seen.has(words[i])) {
+                let diff = 0;
+                for (let j = 0; j < target.length; j++) {
+                    if (currentWord[j] !== words[i][j]) {
+                        diff++;
+                    }
+                }
+                if (diff === 1) {
+                    seen.add(words[i]);
+                    queue.push([words[i], count + 1]);
                 }
             }
         }
     }
-    if (flag) return ans
-    else return 0
+
+    if (found) {
+        return result;
+    } else {
+        return 0;
+    }
 }
