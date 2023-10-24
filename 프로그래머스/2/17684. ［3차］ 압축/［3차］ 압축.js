4a9296503161a87ec9509836f34e2b568d52c90a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-24 12:46:33 +0900

    [level 2] Title: [3차] 압축, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,25 +1,29 @@
 function solution(msg) {
-    const alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const answer = [];
     const dictionary = new Map();
-    [...alp].forEach(v=>{
-        dictionary.set(v, dictionary.size + 1)
-    })
-    let temp = '';
-    for(let i=0; i<msg.length-1; i++) {
-        temp += msg[i]
+    const alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-        if (dictionary.has(temp+msg[i+1])) continue
-        else {
-            answer.push(dictionary.get(temp))
-            dictionary.set(temp+msg[i+1], dictionary.size + 1)
+    [...alp].forEach((v, index) => {
+        dictionary.set(v, index + 1);
+    });
+
+    let temp = '';
+
+    for (let i = 0; i < msg.length; i++) {
+        temp += msg[i];
+
+        if (!dictionary.has(temp + msg[i + 1])) {
+            answer.push(dictionary.get(temp));
+            dictionary.set(temp + msg[i + 1], dictionary.size + 1);
             temp = '';
         }
     }
-    //마지막 처리.
-    answer.push(dictionary.get(temp+msg[msg.length-1]))
-    
-    return answer
+
+    if (temp !== '') {
+        answer.push(dictionary.get(temp));
+    }
+
+    return answer;
 }
 /*
 있으면 붙이고, 없으면 출력하고 붙이고 셋.
