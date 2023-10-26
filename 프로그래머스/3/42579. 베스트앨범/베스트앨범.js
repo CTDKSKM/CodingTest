28d163ff4598aa8f277e575283f388d1985e46d5
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-26 20:56:47 +0900

    [level 3] Title: 베스트앨범, Time: 3.90 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,54 @@
+function solution(genres, plays) {
+    const answer = [];
+
+    const genreMap = {};
+    const indexMap = {};
+
+    genres.forEach((genre, index) => {
+        if (genreMap[genre]) {
+            genreMap[genre].sum += plays[index];
+            genreMap[genre].cnt += 1;
+        } else {
+            genreMap[genre] = { sum: plays[index], cnt: 1 };
+        }
+
+        if (indexMap[genre]) {
+            indexMap[genre].push({ num: plays[index], index });
+        } else {
+            indexMap[genre] = [{ num: plays[index], index }];
+        }
+    });
+
+    for (const genre in indexMap) {
+        indexMap[genre].sort((a, b) => {
+            if (a.num === b.num) {
+                return b.index - a.index
+            }
+            else return a.num - b.num
+        });
+    }
+
+    const sortedGenres = Object.entries(genreMap)
+        .sort((a, b) => b[1].sum - a[1].sum);
+    console.log(genreMap)
+    console.log(indexMap)
+    sortedGenres.forEach(([genre]) => {
+        answer.push(indexMap[genre].pop().index);
+        if (indexMap[genre].length > 0) {
+            answer.push(indexMap[genre].pop().index);
+        }
+    });
+    
+    console.log(sortedGenres)
+    return answer
+}
+
+/*
+
+장르별로 가장 많이 재생된 노래 두개씩 모아 베스트 앨범 출시.
+고유 번호로 구분 index-재생횟수.
+속한 노래가 많이 재생된 장르를 먼저 수록
+장르 내에서 많이 재생된 노래 먼저 수록
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록
+
+*/
