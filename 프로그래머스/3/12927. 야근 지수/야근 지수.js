4f6dd351b895a5489367a519bac7a6d5d4734135
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-27 17:14:56 +0900

    [level 3] Title: 야근 지수, Time: 29.66 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,21 +1,30 @@
 function solution(n, works) {
-    works.sort((a,b)=>a-b)
-    while(n>0) {
-        for(let i=works.length-1; i>=0; i--) {
-            if (n<=0) break
+    if (works.reduce((acc,cur)=>acc+cur,0) <= n) return 0 // n이 충분할 시 0 리턴
+    
+    works.sort((a, b) => b - a); // 가장 많은 작업부터 줄이도록 정렬
 
-            if (works[i] <= works[i-1]) {
-                works[i]--
-                n--
-                continue
+    while (n > 0) {
+        let max = works[0]; // 현재 가장 많은 작업
+        let idx = 0; // 현재 가장 많은 작업의 인덱스
+
+        // 가장 많은 작업을 최대한 균등하게 줄이기
+        for (let i = 1; i < works.length; i++) {
+            if (works[i] >= max) {
+                idx = i;
+                max = works[i];
+            } else {
+                break;
             }
-            else {
-                works[i]--
-                n--
-                break
-            }
+        }
+
+        // 작업량 감소
+        for (let i = 0; i <= idx; i++) {
+            if (n === 0) break;
+            works[i]--;
+            n--;
         }
     }
 
-    return works.some(v=>v<0) ? 0 : works.map(v=>Math.pow(v,2)).reduce((acc,cur)=>acc+cur,0)
+    return works.reduce((acc, cur) => acc + cur * cur, 0);
+
 }
