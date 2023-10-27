030c490d1b7492e534f0bc861dcbada10ff436bb
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-27 16:57:52 +0900

    [level 3] Title: 야근 지수, Time: 5.86 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,25 @@
+function solution(n, works) {
+    works.sort((a,b)=>a-b)
+    while(n>0) {
+        for(let i=works.length-1; i>=0; i--) {
+            if (n<=0) break
+
+            if (works[i] <= works[i-1]) {
+                works[i]--
+                n--
+                continue
+            }
+            else {
+                works[i]--
+                n--
+                break
+            }
+        }
+    }
+
+    return works.some(v=>v<0) ? 0 : works.map(v=>Math.pow(v,2)).reduce((acc,cur)=>acc+cur,0)
+}
+
+/*
+가장 큰걸 차례대로 깎음.
+*/
