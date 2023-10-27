bd60535c70e15e968ede8624a11c6bfc6464fd7c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-27 17:00:58 +0900

    [level 3] Title: 야근 지수, Time: 34.02 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,68 +1,21 @@
-class MaxHeap {
-  constructor() {
-    this.heap = [null];
-  }
-
-  push(value) {
-    this.heap.push(value); // 마지막 트리에 추가
-    let currentIndex = this.heap.length - 1;
-    let parentIndex = Math.floor(currentIndex / 2);
-
-    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
-      const temp = this.heap[parentIndex];
-      this.heap[parentIndex] = value;
-      this.heap[currentIndex] = temp;
-
-      currentIndex = parentIndex;
-      parentIndex = Math.floor(currentIndex / 2);
-    }
-  }
-
-  pop() {
-    if (this.heap.length === 2) return this.heap.pop(); // 루트 정점만 남은 경우
-
-    const returnValue = this.heap[1];
-    this.heap[1] = this.heap.pop();
-
-    let currentIndex = 1;
-    let leftIndex = 2;
-    let rightIndex = 3;
-    while (
-      this.heap[currentIndex] < this.heap[leftIndex] ||
-      this.heap[currentIndex] < this.heap[rightIndex]
-    ) {
-      if (this.heap[leftIndex] < this.heap[rightIndex]) {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[rightIndex];
-        this.heap[rightIndex] = temp;
-        currentIndex = rightIndex;
-      } else {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[leftIndex];
-        this.heap[leftIndex] = temp;
-        currentIndex = leftIndex;
-      }
-      leftIndex = currentIndex * 2;
-      rightIndex = currentIndex * 2 + 1;
-    }
-    return returnValue;
-  }
-}
-
 function solution(n, works) {
-  const heap = new MaxHeap();
+    works.sort((a,b)=>a-b)
+    while(n>0) {
+        for(let i=works.length-1; i>=0; i--) {
+            if (n<=0) break
 
-  for (let i = 0; i < works.length; i++) {
-    heap.push(works[i]);
-  }
+            if (works[i] <= works[i-1]) {
+                works[i]--
+                n--
+                continue
+            }
+            else {
+                works[i]--
+                n--
+                break
+            }
+        }
+    }
 
-  for (let i = 0; i < n; i++) {
-    let peekValue = heap.pop() - 1;
-    if (peekValue < 0) peekValue = 0;
-    heap.push(peekValue);
-  }
-
-  return heap.heap
-    .filter((item) => item !== null)
-    .reduce((acc, item) => acc + item ** 2, 0);
+    return works.some(v=>v<0) ? 0 : works.map(v=>Math.pow(v,2)).reduce((acc,cur)=>acc+cur,0)
 }
