19301c3b17dcbca4df82e6ad707de6a96b609aa8
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-10-30 20:27:38 +0900

    [level 2] Title: 땅따먹기, Time: 50.37 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,13 +1,19 @@
 function solution(land) {
-    for(let i=1; i<land.length; i++) {
-        land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3])
-        land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3])
-        land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3])
-        land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2])
-
+    const removeColumnUsed = (arr, index) => arr[index] = 0
+    const getMaxScore = (row, index) => {
+        const rowCopy = [...row]
+        removeColumnUsed(rowCopy, index)
+        return Math.max(...rowCopy)
     }
-
-    return Math.max(...land.at(-1));
+    
+    for(let i = 1; i < land.length; i++) {
+        land[i][0] += getMaxScore(land[i-1], 0)
+        land[i][1] += getMaxScore(land[i-1], 1)
+        land[i][2] += getMaxScore(land[i-1], 2)
+        land[i][3] += getMaxScore(land[i-1], 3)
+    }
+    
+    return Math.max(...land.at(-1))
 }
 
 /*
