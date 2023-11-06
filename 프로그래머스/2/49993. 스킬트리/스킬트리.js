9270ccdd07509fc350a7431cde330e38a295b379
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-06 19:39:29 +0900

    [level 2] Title: 스킬트리, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,20 +1,31 @@
 function solution(skill, skill_trees) {
-    var answer = 0;
-    return vaildSkillTree(skill, skill_trees);
+    const memo = new Map();
+
+    skill_trees.forEach((tree,i)=>{
+        if (tree.includes(skill[0])) memo.set(i, tree.indexOf(skill[0]))
+        else memo.set(i, 27)
+    })
+
+    for(let i=1; i<skill.length; i++) {
+        const nowSkill = skill[i]
+        for(let j=0; j<skill_trees.length; j++) {
+            const tree = skill_trees[j]
+            const index = tree.indexOf(nowSkill)
+            if (index == -1) {
+                for(let k=i+1; k<skill.length; k++) {
+                    const nextSkill = skill[k]
+                    if (tree.includes(nextSkill)) {
+                        memo.delete(j)
+                        break
+                    }
+                }
+                continue
+            }
+
+            if (memo.get(j) < index) memo.set(j, index)
+            else memo.delete(j)
+        }
+    }
+
+    return memo.size;
 }
-
-
-const vaildSkillTree = (skillTree, usersSkillTrees)=>{
-
-    const onlYneedSkills = usersSkillTrees.map(it=>{
-        return it.split('').filter(ch => skillTree.includes(ch)).join('');
-    });
-
-    const filter = onlYneedSkills.map(element => {
-        if(element === '') return true
-        if(element[0] != skillTree[0]) return false
-        return skillTree.includes(element);
-    });
-
-    return filter.filter(it => it).length
-};
