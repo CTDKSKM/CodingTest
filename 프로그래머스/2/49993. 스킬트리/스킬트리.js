c525dc3a1bfe307e6764e323f39cbab92ed17d5f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-06 19:33:11 +0900

    [level 2] Title: 스킬트리, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,36 +1,11 @@
 function solution(skill, skill_trees) {
-    const memo = new Map();
+    var answer = 0;
+    var regex = new RegExp(`[^${skill}]`, 'g');
 
-    skill_trees.forEach((tree, i) => {
-        const index = tree.indexOf(skill[0]);
-        memo.set(i, index > -1 ? index : 27);
-    });
-
-    for (let i = 1; i < skill.length; i++) {
-        const nowSkill = skill[i];
-        for (let j = 0; j < skill_trees.length; j++) {
-            const tree = skill_trees[j];
-            const index = tree.indexOf(nowSkill);
-            if (index > -1) {
-                memo.get(j) < index ? memo.set(j, index) : memo.delete(j);
-            } else {
-                for (let k = i + 1; k < skill.length; k++) {
-                    if (tree.includes(skill[k])) {
-                        memo.delete(j);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    return memo.size;
+    return skill_trees
+        .map((x) => x.replace(regex, ''))
+        .filter((x) => {
+            return skill.indexOf(x) === 0 || x === "";
+        })
+        .length
 }
-
-
-/*
-skill
-skill_trees에서 skill이 가능한 트리의 개수.
-가장 빠른 스킬의 인덱스가 더 낮으면 가능
-이전스킬이 있는지..
-*/
