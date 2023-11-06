db69a70f72512815bbd63308e84d50e0429563d6
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-06 19:40:13 +0900

    [level 2] Title: 스킬트리, Time: 0.10 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,29 +1,26 @@
 function solution(skill, skill_trees) {
     const memo = new Map();
 
-    skill_trees.forEach((tree,i)=>{
-        if (tree.includes(skill[0])) memo.set(i, tree.indexOf(skill[0]))
-        else memo.set(i, 27)
-    })
+    skill_trees.forEach((tree, i) => {
+        const index = tree.indexOf(skill[0]);
+        memo.set(i, index > -1 ? index : 27);
+    });
 
-    for(let i=1; i<skill.length; i++) {
-        const nowSkill = skill[i]
-        for(let j=0; j<skill_trees.length; j++) {
-            const tree = skill_trees[j]
-            const index = tree.indexOf(nowSkill)
-            if (index == -1) {
-                for(let k=i+1; k<skill.length; k++) {
-                    const nextSkill = skill[k]
-                    if (tree.includes(nextSkill)) {
-                        memo.delete(j)
-                        break
+    for (let i = 1; i < skill.length; i++) {
+        const nowSkill = skill[i];
+        for (let j = 0; j < skill_trees.length; j++) {
+            const tree = skill_trees[j];
+            const index = tree.indexOf(nowSkill);
+            if (index > -1) {
+                memo.get(j) < index ? memo.set(j, index) : memo.delete(j);
+            } else {
+                for (let k = i + 1; k < skill.length; k++) {
+                    if (tree.includes(skill[k])) {
+                        memo.delete(j);
+                        break;
                     }
                 }
-                continue
             }
-
-            if (memo.get(j) < index) memo.set(j, index)
-            else memo.delete(j)
         }
     }
 
