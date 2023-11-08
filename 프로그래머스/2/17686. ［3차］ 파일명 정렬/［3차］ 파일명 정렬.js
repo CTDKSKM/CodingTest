e3ecc2b7c820ea2b86723b9883fcc0d6bc60816b
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-08 20:46:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 30.78 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,54 +1,60 @@
 function solution(files) {
-    const NUMBER = '0123456789'
-
-    files = files.map(v=>{
-        const result = []
-        let numIdx;
-        
-        for(let i=0; i<v.length; i++) {
-            if (!result.length && NUMBER.includes(v[i])) {
-                numIdx = i
-                result.push(v.slice(0, i))
+    function filename(){
+        this.head = [];
+        this.num = [];
+        this.tail = [];
+    }
+    var answer = []; 
+    var answer2=[];
+    var i=0,j=0,sw=0
+    var c=0,c1=0,c2=0;
+    for(i=files.length-1;i>=0;i--){
+        answer[i]=new filename;
+        for(j=0;j<files[i].length;j++){
+            if('0'<=files[i][j]&&files[i][j]<='9'){
+                if(sw==0){
+                    c1=j;
+                    answer[i].head=files[i].slice(0,j);
+                    sw=1;
+                }
             }
-            else if (result.length && !NUMBER.includes(v[i])) {
-                result.push(v.slice(numIdx, i))
-                result.push(v.slice(i))
-                break
+            else if(sw==1){
+                c2=j;
+                answer[i].num=files[i].slice(c1,j);
+                sw=2;
             }
-            else if (result.length && i == v.length-1) {
-                result.push(v.slice(numIdx, i+1))
-                result.push(v.slice(i+1))
-                break
-            }
-            else if (i - numIdx >= 5) {
-                result.push(v.slice(numIdx, i))
-                result.push(v.slice(i))
-                break
-            }
-            
-            
         }
+        if(sw==1)
+            answer[i].num=files[i].slice(c1,j);
+        if(sw==2)
+            answer[i].tail=files[i].slice(c2,j);
+        sw=0;
+    }
 
-        if (result.length == 1) result.push(v.slice(numIdx))
-
-        return result
-    }).sort((a,b)=>{
-        const a_h = a[0].toLowerCase()
-        const b_h = b[0].toLowerCase()
-        const a_n = Number(a[1])
-        const b_n = Number(b[1])
-        
-        if (a_h > b_h) return 1
-        else if (a_h == b_h) {
-            if (a_n > b_n) return 1
-            else if (a_n == b_n) {
-                return 0
+    var tm = new filename;
+    for(i=1;i<files.length;i++){
+        for(j=0;j<files.length-i;j++){
+            if(answer[j].head.toLowerCase()>answer[j+1].head.toLowerCase()){
+                tm=answer[j+1];
+                answer[j+1]=answer[j];
+                answer[j]=tm;
             }
-            else return -1
+            else if(answer[j+1].head.toLowerCase()==answer[j].head.toLowerCase()){
+                if(Number(answer[j].num)>Number(answer[j+1].num)){
+                    tm=answer[j+1];
+                    answer[j+1]=answer[j];
+                    answer[j]=tm;
+                }
+            }
         }
-        else return -1
-    }).map(v=>v.join(''));
-    return files
+    }
+
+    //console.log(answer[2].head,answer[2].num,answer[2].tail);
+    for(i=0;i<files.length;i++)
+        answer2[i]=answer[i].head+answer[i].num+answer[i].tail;
+
+    //console.log(files.sort(/*(a,b) => (a-b)*/));
+    return answer2;
 }
 /*
 단순한 문자 코드 순이 아닌, 파일명에 포함된 숫자를 반영한 정렬 기능을 저장소 관리 프로그램에 구현하기로
