5c1c1650e5a1e9e4053a28fcef38074529e9a890
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-12 03:17:52 +0900

    [level 2] Title: 택배상자, Time: 16.26 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,36 @@
+function solution(order) {
+    var answer = 0;
+    let count = 0;
+    const stack = []
+    let flag = false
+    while (1) {
+        for(let i=0; i<order.length; i++) {
+            if (i==order.length-1) flag = true
+            const nextBox = order[i]
+            
+            if (count < nextBox) {
+                while (count < nextBox-1) {
+                    stack.push(count+1)
+                    count++
+                }
+                answer++
+                count++
+            }
+            else {
+                if (stack.pop() == nextBox) {
+                    answer++
+                }
+                else {
+                    flag = true
+                    break
+                }
+            }
+        }
+        if (flag) break
+    }
+
+    return answer;
+}
+/*
+
+*/
