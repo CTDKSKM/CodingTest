a109696c32a8e338af9c6bd1d807f659e39f57e9
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-11 20:40:25 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.46 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,27 +1,221 @@
 function solution(numbers, hand) {
-    hand = hand[0] === "r" ? "R" : "L"
-  let position = [1, 4, 4, 4, 3, 3, 3, 2, 2, 2]
-  let h = { L: [1, 1], R: [1, 1] }
-  return numbers.map(x => {
-    if (/[147]/.test(x)) {
-      h.L = [position[x], 1]
-      return "L"
+    const locator = {left:'*', right:'#'}
+
+    return numbers.map((num)=>{
+        if (num == 1 || num == 4 || num == 7) {
+            locator.left = num;
+            return "L"
+        }
+        else if (num == 3 || num == 6 || num == 9) {
+            locator.right = num;
+            return "R"
+        }
+        else {
+            const leftDist = getLeft(locator.left, num)
+            const rightDist = getRight(locator.right, num)
+            if (leftDist < rightDist) {
+                locator.left = num
+                return "L"
+            }
+            else if (leftDist > rightDist) {
+                locator.right = num
+                return "R"
+            }
+            else {
+                if (hand == 'left') {
+                    locator.left = num
+                    return "L"
+                }
+                else {
+                    locator.right = num
+                    return "R"
+                }
+            }
+        }
+    }).join('');
+}
+
+function getLeft(locate, target) {
+    switch(locate) {
+        case '*':
+            switch(target) {
+                case 2:
+                    return 4
+                case 5:
+                    return 3
+                case 8:
+                    return 2
+                case 0:
+                    return 1
+            }
+        case 1:
+            switch(target) {
+                case 2:
+                    return 1
+                case 5:
+                    return 2
+                case 8:
+                    return 3
+                case 0:
+                    return 4
+            }
+        case 2:
+            switch(target) {
+                case 2:
+                    return 0
+                case 5:
+                    return 1
+                case 8:
+                    return 2
+                case 0:
+                    return 3
+            }
+        case 4:
+            switch(target) {
+                case 2:
+                    return 2
+                case 5:
+                    return 1
+                case 8:
+                    return 2
+                case 0:
+                    return 3
+            }
+        case 5:
+            switch(target) {
+                case 2:
+                    return 1
+                case 5:
+                    return 0
+                case 8:
+                    return 1
+                case 0:
+                    return 2
+            }
+        case 7:
+            switch(target) {
+                case 2:
+                    return 3
+                case 5:
+                    return 2
+                case 8:
+                    return 1
+                case 0:
+                    return 2
+            }
+        case 8:
+            switch(target) {
+                case 2:
+                    return 2
+                case 5:
+                    return 1
+                case 8:
+                    return 0
+                case 0:
+                    return 1
+            }
+        case 0:
+            switch(target) {
+                case 2:
+                    return 3
+                case 5:
+                    return 2
+                case 8:
+                    return 1
+                case 0:
+                    return 0
+            }
     }
-    if (/[369]/.test(x)) {
-      h.R = [position[x], 1]
-      return "R"
+}
+function getRight(locate, target) {
+    switch(locate) {
+        case '#':
+            switch(target) {
+                case 2:
+                    return 4
+                case 5:
+                    return 3
+                case 8:
+                    return 2
+                case 0:
+                    return 1
+            }
+        case 0:
+            switch(target) {
+                case 2:
+                    return 3
+                case 5:
+                    return 2
+                case 8:
+                    return 1
+                case 0:
+                    return 0
+            }
+        case 2:
+            switch(target) {
+                case 2:
+                    return 0
+                case 5:
+                    return 1
+                case 8:
+                    return 2
+                case 0:
+                    return 3
+            }
+        case 3:
+            switch(target) {
+                case 2:
+                    return 1
+                case 5:
+                    return 2
+                case 8:
+                    return 3
+                case 0:
+                    return 4
+            }
+        case 5:
+            switch(target) {
+                case 2:
+                    return 1
+                case 5:
+                    return 0
+                case 8:
+                    return 1
+                case 0:
+                    return 2
+            }
+        case 6:
+            switch(target) {
+                case 2:
+                    return 2
+                case 5:
+                    return 1
+                case 8:
+                    return 2
+                case 0:
+                    return 3
+            }
+        case 8:
+            switch(target) {
+                case 2:
+                    return 2
+                case 5:
+                    return 1
+                case 8:
+                    return 0
+                case 0:
+                    return 1
+            }
+        case 9:
+            switch(target) {
+                case 2:
+                    return 3
+                case 5:
+                    return 2
+                case 8:
+                    return 1
+                case 0:
+                    return 2
+            }
     }
-    let distL = Math.abs(position[x] - h.L[0]) + h.L[1]
-    let distR = Math.abs(position[x] - h.R[0]) + h.R[1]
-    if (distL === distR) {
-      h[hand] = [position[x], 0]
-      return hand
-    }
-    if (distL < distR) {
-      h.L = [position[x], 0]
-      return "L"
-    }
-    h.R = [position[x], 0]
-    return "R"
-  }).join("")
 }
