e3e9df442b0a03eea93f4ff5a8630c8d88707e56
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-12 03:25:01 +0900

    [level 2] Title: 택배상자, Time: 14.45 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,35 +1,21 @@
 function solution(order) {
-    var answer = 0;
-    let count = 0;
-    const stack = []
-    let flag = false
-    while (1) {
-        for(let i=0; i<order.length; i++) {
-            if (i==order.length-1) flag = true
-            const nextBox = order[i]
-            
-            if (count < nextBox) {
-                while (count < nextBox-1) {
-                    stack.push(count+1)
-                    count++
-                }
-                answer++
-                count++
-            }
-            else {
-                if (stack.pop() == nextBox) {
-                    answer++
-                }
-                else {
-                    flag = true
-                    break
-                }
-            }
+    let k = [], c = 1, count = 0
+    for(let i = 0; i < order.length; ++i){
+        let t = order[i]
+        if(k.length && k[k.length-1]==t){
+            k.pop()
+            ++count
+            continue
         }
-        if (flag) break
+        while(t!=c){
+            k.push(c)
+            ++c
+            if(c>order.length) return count
+        }
+        ++c
+        ++count
     }
-
-    return answer;
+    return count
 }
 /*
 
