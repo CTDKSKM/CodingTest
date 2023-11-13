3bff0dbced1bb910ba9b12cb53760765028625a4
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-13 13:59:57 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,15 +1,13 @@
 function solution(ingredient) {
-    let answer = 0
-    let stack = []
-    ingredient.forEach((inner)=>{
-        stack.push(inner)
-        const sLength = stack.length
-        if(stack[sLength-4] === 1 && stack[sLength-3] === 2 &&  stack[sLength-2] === 3 && stack[sLength-1] === 1){
-            for(let i=0;i<4;i++){
-                stack.pop()
-            }
-            answer++
-         } 
-    })
-    return answer
+    let count = 0;
+
+    for (let i = 0; i < ingredient.length; i++) {
+        if (ingredient.slice(i, i + 4).join('') === '1231') {
+            count++;
+            ingredient.splice(i, 4);
+            i -= 3;
+        }
+    }
+
+    return count;
 }
