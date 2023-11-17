f0c14bd817ae74a80cbf8197e2adc185a3e75136
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-18 00:20:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 192 ms, Memory: 11700 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -2,19 +2,19 @@ const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
 const n = +input[0];
-const answer = [];
-const dp = {};
-dp[3] = 1;
-dp[5] = 1;
-let arr = [3, 5];
-if (n == 3 || n == 5) answer.push(1);
-else {
+
+function solution(n) {
+  const dp = {};
+  dp[3] = 1;
+  dp[5] = 1;
+  let arr = [3, 5];
+  if (n == 3 || n == 5) return 1;
   while (arr.length) {
     const arr2 = [];
     for (const d of arr) {
       for (const e of [d + 3, d + 5]) {
         if (e > n || dp[e]) continue;
-        if (e == n) answer.push(dp[d] + 1);
+        if (e == n) return dp[d] + 1;
         dp[e] = dp[d] + 1;
 
         arr2.push(e);
@@ -22,7 +22,8 @@ else {
     }
     arr = arr2;
   }
+
+  return -1;
 }
 
-if (answer.length) console.log(answer[0]);
-else console.log(-1);
+console.log(solution(n));
