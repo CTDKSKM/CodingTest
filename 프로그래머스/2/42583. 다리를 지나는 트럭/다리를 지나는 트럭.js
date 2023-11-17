aae5458134281ea78cd9a403082d0ee3e78e0f5a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-18 06:14:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,35 +1,26 @@
-function solution(b_len, max, waits) {
-    waits = waits.reverse().map(truck=>({weight:truck, time:b_len}));
-    let through = [];
-    let weight = 0;
-    let cnt = 0;
-    
-    while(waits.length || through.length) {
-        if (waits.length && max >= weight + waits.at(-1).weight && b_len > through.length) {
-            const truck = waits.pop()
-            through.push(truck)
-            weight += truck.weight
-            through = through.filter((truck)=>{
-                truck.time--
-                if (truck.time!==0) return true
-                else {
-                    weight -= truck.weight
-                    return false
-                }
-            })
-            cnt++
+function solution(bridge_length, weight, truck_weights) {
+    var answer = 0;
+    let total_truck_weight = 0;
+    let bridge_queue = [], weight_queue = [];
+
+    do {
+        // 다리를 건너는 트럭 이동
+        for(let i in bridge_queue) {
+            bridge_queue[i]--;
         }
-        else {
-            through = through.filter((truck)=>{
-                truck.time--
-                if (truck.time!==0) return true
-                else {
-                    weight -= truck.weight
-                    return false
-                }
-            })
-            cnt++
+        if(bridge_queue[0] == 0) {
+            total_truck_weight -= weight_queue.shift();
+            bridge_queue.shift();
         }
-    }
-    return cnt+1
+
+        // 다리가 견딜 수 있으면 트럭 1개 올리기
+        if(total_truck_weight + truck_weights[0] <= weight) {
+            weight_queue.push(truck_weights[0]);
+            bridge_queue.push(bridge_length);
+            total_truck_weight += truck_weights.shift();
+        }
+        answer++;
+    } while(bridge_queue.length > 0)
+
+    return answer;
 }
