7cafcd5100be7d5733d0ad5e8d8282a8b7bf3db7
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-18 06:24:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,31 +1,35 @@
 function solution(b_len, max, waits) {
     waits = waits.reverse().map(truck=>({weight:truck, time:b_len}));
     let through = [];
-    let weight = {val: 0};
+    let weight = 0;
     let cnt = 0;
 
     while(waits.length || through.length) {
-        if (waits.length && max >= weight.val + waits.at(-1).weight && b_len > through.length) {
+        if (waits.length && max >= weight + waits.at(-1).weight && b_len > through.length) {
             const truck = waits.pop()
             through.push(truck)
-            weight.val += truck.weight
-            through = move(through, weight)
+            weight += truck.weight
+            through = through.filter((truck)=>{
+                truck.time--
+                if (truck.time!==0) return true
+                else {
+                    weight -= truck.weight
+                    return false
+                }
+            })
             cnt++
         }
         else {
-            through = move(through, weight)
+            through = through.filter((truck)=>{
+                truck.time--
+                if (truck.time!==0) return true
+                else {
+                    weight -= truck.weight
+                    return false
+                }
+            })
             cnt++
         }
     }
     return cnt+1
-}
-function move(arr, weight) {
-    return arr.filter((truck)=>{
-                truck.time--
-                if (truck.time!==0) return true
-                else {
-                    weight.val -= truck.weight
-                    return false
-                }
-        })
 }
