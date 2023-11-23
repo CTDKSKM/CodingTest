76c9c3ff1467a13e69f9c7920e9a6813877e01bf
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-11-23 18:26:16 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 11.43 ms, Memory: 44.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,32 +1,23 @@
 function solution(queue1, queue2) {
-    const L = queue1.reduce((a,c)=>a+c,0)
-    const R = queue2.reduce((a,c)=>a+c,0)
-    const arr = L > R ? queue2.concat(queue1) : queue1.concat(queue2)
-    const goal = arr.reduce((a,c)=>a+c,0) / 2
-    let left = 0;
-    let right = queue1.length-1;
-    let count = 0;
-    let sum = 0;
-
-    for (let i = left; i<=right; i++) {
-        sum += arr[i]
-    }
-
-    while (count <= queue1.length*4) {
-        if (sum === goal) return count
-        
-        if (sum > goal) {
-            arr.push(arr[left]);
-            sum-=arr[left++];
+    let answer = 0
+    const max = queue1.length*2
+    let value = queue1.reduce((acc,cur,idx)=>acc+cur-queue2[idx],0)/2
+    let [i,j] = [0,0]
+    while(value !== 0 && answer < max*2){
+        if(value > 0){
+            const v = queue1[i]
+            i++
+            value -= v
+            queue2.push(v)
+        }else{
+            const v = queue2[j]
+            j++
+            value += v
+            queue1.push(v)
         }
-        else if (sum < goal) {
-            sum+=arr[++right];
-        }
-
-        count++
+        answer++
     }
-    
-    return -1;
+    return value !== 0 ? -1 : answer
 }
 
 /*
