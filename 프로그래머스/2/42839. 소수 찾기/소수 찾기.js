9006f39448c1067496401d0894d9eacdabd59439
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-03 18:42:48 +0900

    [level 2] Title: 소수 찾기, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,39 +1,30 @@
 function solution(numbers) {
-    var answer = 0;
+    const result = new Set();
+    const used = Array(numbers.length).fill(false);
 
-    var n = numbers.split('');
-    var nums = new Set()
-    combi(n,'');
-
-    function combi(a, s) {
-        if (s.length > 0) {
-            if (nums.has(Number(s))=== false) {
-                nums.add(Number(s));
-
-                if (chkPrime(Number(s))) {
-
-                    answer++;
-                }
-            }
-        }
-        if (a.length > 0) {
-            for (var i = 0; i< a.length; i++) {
-                var t = a.slice(0)
-                t.splice(i,1);
-
-                combi(t,s + a[i]);
-            }
-        }
+    for(let i=1; i<=numbers.length; i++) {
+        permutation(0, result, numbers, "", i, used)
     }
 
-    function chkPrime(num) {
-        if (num < 2) return false;
-        if (num === 2) return true;
-        for (var i = 2; i <= Math.sqrt(num); i++) {
-            if (num%i===0) return false;
-        }
-        return true;
+    return Array.from(result).filter(num => checkPrime(parseInt(num))).length;
+}
+function checkPrime(number) {
+    if (number <= 1) return false
+    for(let i=2; i*i<=number; i++) {
+        if(number % i == 0) return false
+    }
+    return true
+}
+function permutation(count, result, str, temp, length, used) {
+    if (count == length) {
+        result.add(parseInt(temp))
+        return
     }
 
-    return answer;
+    for(let i=0; i<str.length; i++) {
+        if (used[i]) continue;
+        used[i] = true;
+        permutation(count+1, result, str, temp+str[i], length, used);
+        used[i] = false
+    }
 }
