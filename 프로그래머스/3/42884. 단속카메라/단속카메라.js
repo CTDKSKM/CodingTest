4bb1c2e4dc00eea1190a5f3e73d004add3eca2f4
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-06 19:11:16 +0900

    [level 3] Title: 단속카메라, Time: 29.55 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,12 +1,12 @@
 function solution(routes) {
-    let cnt = 0;
-  let camera = -30001;
-  routes.sort((a,b)=>a[1]-b[1]);
-  for(let item of routes){
-    if(item[0]>camera){
-      cnt++;
-      camera=item[1];
-    }
-  }
- return cnt;
+    let count = 0;
+    let temp = -Infinity;
+    routes.sort((a,b)=>a[1]-b[1]);
+    routes.forEach(([s,e])=>{
+        if (s > temp) {
+            count++
+            temp = e
+        }
+    })
+    return count
 }
