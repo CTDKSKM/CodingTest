eefee20e73b3894f5e7238e0c07ccc511785ea67
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-10 01:00:52 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 18.86 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,85 +1,15 @@
 function solution(arr) {
-    const answer = [0, 0];
-    let $ = quadify(arr, answer)
-    let len = arr.length;
-    let cnt = 0;
-    while (len > 1) {
-        cnt++
-        len = len/2
-    }
-    
-    try {
-        $ = $.flat(cnt+1)
-    } catch {
-        return answer
-    }
-
-    $.forEach(val=>{
-        if (val > -1) answer[val]++
-    })
-    return answer
-}
-function quadify(arr, ans) {
-    if (arr.length == 1) return arr
-
-    let checker = arr[0][0]
-    let isSame = true
-    arr.forEach((checkedArr)=>{
-        checkedArr.forEach(val=>{
-            if (val != checker) isSame = false
-        })
-    })
-    if (isSame) {
-        ans[checker]++
-        return -1
-    }
-
-    const len = arr.length
-
-    const newQuadArr = Array.from({length:4},()=>([]))
-
-    return newQuadArr.map((_,idx)=>{
-        const newArr = Array.from({length:len/2},()=>([]))
-        let iter = len/2
-        let n_i = 0
-        while(n_i < iter) {
-            if (idx == 0) {
-                for(let i=0; i<iter; i++) {
-                    for(let j=0; j<iter; j++) {
-                        if (i == n_i)
-                        newArr[n_i].push(arr[i][j])
-                    }
-                }
-            }
-            else if (idx == 1) {
-                for(let i=0; i<iter; i++) {
-                    for(let j=iter; j<len; j++) {
-                        if (i == n_i)
-                        newArr[n_i].push(arr[i][j])
-                    }
-                }
-            }
-            else if (idx == 2) {
-                for(let i=iter; i<len; i++) {
-                    for(let j=0; j<iter; j++) {
-                        if (i == iter + n_i && j < iter) {
-                            newArr[n_i].push(arr[i][j])
-                        }
-                    }
-                }
-            }
-            else {
-                for(let i=iter; i<len; i++) {
-                    for(let j=iter; j<len; j++) {
-                        if (i == iter + n_i && j >= iter) {
-                            newArr[n_i].push(arr[i][j])
-                        }
-                    }
-                }
-            }
-            n_i++
+    const quadZip = (row, col, n) => {
+        if(n < 2) return arr[row][col] ? [0, 1] : [1, 0];
+        let cnt0 = 0, cnt1 = 0; n >>= 1;
+        for(let [i, j] of [[0,0],[0,1],[1,0],[1,1]]) {
+            let [zero, one] = quadZip(row+i*n, col+j*n, n);
+            cnt0 += zero;
+            cnt1 += one;
         }
-
-        return quadify(newArr, ans)
-    })
+        if(cnt0 === 0) return [0, 1];
+        if(cnt1 === 0) return [1, 0];
+        return [cnt0, cnt1];
+    }
+    return quadZip(0, 0, arr.length);
 }
