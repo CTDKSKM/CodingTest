e56b7f0e2f65f3a7b8849368371effb449320bd9
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-10 00:30:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.70 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,84 +1,25 @@
+var cnt_0 = 0, cnt_1 = 0;
 function solution(arr) {
-    const answer = [0, 0];
-    let $ = quadify(arr, answer)
-    let len = arr.length;
-    let cnt = 0;
-    while (len > 1) {
-        cnt++
-        len = len/2
-    }
-    try {
-        $ = $.flat(cnt+1)
-    } catch {
-        return answer
-    }
-    
-    $.forEach(val=>{
-        if (val > -1) answer[val]++
-    })
-    return answer
+    checkArea(arr, 0, arr.length, 0, arr.length);
+    return [cnt_0, cnt_1];
 }
-function quadify(arr, ans) {
-    if (arr.length == 1) return arr
-    
-    let checker = arr[0][0]
-    let isSame = true
-    arr.forEach((checkedArr)=>{
-        checkedArr.forEach(val=>{
-            if (val != checker) isSame = false
-        })
-    })
-    if (isSame) {
-        ans[checker]++
-        return
-    }
-    
-    const len = arr.length
-    
-    const newQuadArr = Array.from({length:4},()=>([]))
-
-    return newQuadArr.map((_,idx)=>{
-        const newArr = Array.from({length:len/2},()=>([]))
-        let iter = len/2
-        let n_i = 0
-        while(n_i < iter) {
-            if (idx == 0) {
-                for(let i=0; i<iter; i++) {
-                    for(let j=0; j<iter; j++) {
-                        if (i == n_i)
-                        newArr[n_i].push(arr[i][j])
-                    }
-                }
-            }
-            else if (idx == 1) {
-                for(let i=0; i<iter; i++) {
-                    for(let j=iter; j<len; j++) {
-                        if (i == n_i)
-                        newArr[n_i].push(arr[i][j])
-                    }
-                }
-            }
-            else if (idx == 2) {
-                for(let i=iter; i<len; i++) {
-                    for(let j=0; j<iter; j++) {
-                        if (i == iter + n_i && j < iter) {
-                            newArr[n_i].push(arr[i][j])
-                        }
-                    }
-                }
-            }
-            else {
-                for(let i=iter; i<len; i++) {
-                    for(let j=iter; j<len; j++) {
-                        if (i == iter + n_i && j >= iter) {
-                            newArr[n_i].push(arr[i][j])
-                        }
-                    }
-                }
-            }
-            n_i++
+function checkArea(arr, row_min, row_max, col_min, col_max) {
+    var cnt = 0;
+    for(var row = row_min; row < row_max; row++) {
+        for(var col = col_min; col < col_max; col++) {
+            if(arr[row][col]) cnt++;
         }
-        
-        return quadify(newArr, ans)
-    })
+    }
+    if(cnt == 0) {
+        cnt_0 += 1;
+    } else if((row_max - row_min) * (col_max - col_min) == cnt) {
+        cnt_1 += 1;
+    } else {
+        var new_row = (row_min + row_max) / 2;
+        var new_col = (col_min + col_max) / 2;
+        checkArea(arr, row_min, new_row, col_min, new_col);
+        checkArea(arr, row_min, new_row, new_col, col_max);
+        checkArea(arr, new_row, row_max, col_min, new_col);
+        checkArea(arr, new_row, row_max, new_col, col_max);
+    }
 }
