5c81452ce0b498a4e1fa812aa517a3ec04096bbb
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-14 10:01:01 +0900

    [level 2] Title: 요격 시스템, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,12 +1,14 @@
 function solution(targets) {
-    var answer = 0;
-    let locate = -Infinity
-    targets.sort((a,b)=>a[1]-b[1])
-    targets.forEach(([s,e])=>{
-        if (locate <= s) {
-            answer++
-            locate = e
+    let answer = 0;
+    const tgts = targets.slice().sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+    let tail = -1;
+    tgts.forEach(([s, e]) => {
+        if (tail <= s) {
+            answer++;
+            tail = e;
+        } else if (e < tail) {
+            tail = e;
         }
-    })
+    });
     return answer;
 }
