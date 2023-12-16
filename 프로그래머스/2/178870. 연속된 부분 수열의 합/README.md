3d4aab47978bc107c4490d5a2550d24fb4bd09a4
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-16 23:26:15 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 23:21:18
+2023년 12월 6일 23:26:9
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,20 +1,26 @@
 function solution(sequence, k) {
-    var answer = [0, sequence.length-1];
+    let answer = [0, sequence.length - 1];
     let sum = 0;
     let left = 0;
     let right = 0;
-    for(let i=0; i<sequence.length; i++) {
-        const num = sequence[i]
-        sum+=num
-        right = i
-        while(sum > k) {
-            sum -= sequence[left]
-            left += 1
+
+    for (let i = 0; i < sequence.length; i++) {
+        const num = sequence[i];
+        sum += num;
+        right = i;
+
+        while (sum > k) {
+            sum -= sequence[left];
+            left += 1;
         }
-        if (sum == k && answer[1]-answer[0] > right - left) {
-            answer[0] = left
-            answer[1] = right
+
+        const currentLength = right - left;
+        const bestLength = answer[1] - answer[0];
+
+        if (sum === k && currentLength < bestLength) {
+            answer = [left, right];
         }
     }
+
     return answer;
 }
