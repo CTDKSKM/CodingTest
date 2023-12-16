a77dc0921e0ee880b8a83b419a6a9670dd61123f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-16 23:55:58 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.08 ms
+메모리: 33.6 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 23:54:40
+2023년 12월 6일 23:55:52
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,27 +1,6 @@
 function solution(storey) {
-    var answer = 0;
-    let arr = [...storey.toString()].map(val=>+val)
-    for(let i=arr.length-1; i>=0; i--) {
-        if (arr[i] > 5) {
-            answer += 10-arr[i]
-            if (i>0) {
-                arr[i-1]++
-            }
-            else answer++
-        }
-        else if (arr[i] < 5) {
-            answer += arr[i]
-        }
-        else if (arr[i] == 5) {
-            if (arr[i-1] > 4) {
-                answer+=10-arr[i]
-                if (i>0) {
-                    arr[i-1]++
-                }
-                else answer++
-            }
-            else answer += arr[i]
-        }
-    }
-    return answer;
+    if (storey < 5) return storey;
+    const r = storey % 10;
+    const m = (storey - r) / 10;
+    return Math.min(r + solution(m), 10 - r + solution(m + 1));
 }
