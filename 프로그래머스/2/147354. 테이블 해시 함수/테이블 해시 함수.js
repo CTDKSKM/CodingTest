8f3438ee24a780df3c79d1652dbc39041f7bc3de
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2023-12-25 17:28:08 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,31 +1,13 @@
 function solution(data, col, row_begin, row_end) {
-    let bit = '';
-
-    data.sort((a, b) => {
-        if (a[col - 1] > b[col - 1]) {
-            return 1;
-        } else if (a[col - 1] === b[col - 1]) {
-            if (b[0] > a[0]) return 1;
-            else return -1;
-        } else return -1;
-    });
-
-    for (let i = row_begin; i <= row_end; i++) {
-        const sum = data[i - 1].reduce((a,c)=>a + (c%i),0);
-        let nowBit = sum.toString(2);
-        if (bit.length) {
-            const len = Math.max(bit.length, nowBit.length);
-            const arr = [];
-            if (bit.length !== len) {
-                bit = '0'.repeat(len - bit.length) + bit;
-            } else {
-                nowBit = '0'.repeat(len - nowBit.length) + nowBit;
-            }
-            for (let j = 0; j < len; j++) {
-                bit[j] !== nowBit[j] ? arr.push('1') : arr.push('0');
-            }
-            bit = arr.join('');
-        } else bit = nowBit;
+    let answer = 0;
+    
+    data.sort((a,b)=>{
+        return a[col - 1] - b[col - 1] || b[0] - a[0]
+    })
+    
+    for(let i=row_begin; i<=row_end; i++) {
+        answer ^= data[i-1].reduce((a,c)=>a+c%i, 0)
     }
-    return parseInt(bit, 2);
+    
+    return answer
 }
