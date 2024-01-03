b5d19e8cfa307babad437627bab3ffc7bf355ad1
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-03 19:20:06 +0900

    [level 2] Title: 삼각 달팽이, Time: 48.49 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,13 +1,38 @@
+const DOWN = 0
+const RIGHT = 1
+const UP = 2
+
 function solution(n) {
-  let a = Array(n).fill().map((_, i) => Array(i + 1).fill())
-  let row = -1
-  let col = 0
-  let fill = 0
-  for (let i = n; i > 0; i -= 3) {
-    a[++row][col] = ++fill
-    for (let j = 0; j < i - 1; j++) a[++row][col] = ++fill
-    for (let j = 0; j < i - 1; j++) a[row][++col] = ++fill
-    for (let j = 0; j < i - 2; j++) a[--row][--col] = ++fill
-  }
-  return a.flat()
+    var answer = [];
+
+    var obj = {}
+    let runnable = (len, rootIdx, init) => {
+        if (init > n * (n + 1) / 2 || len < 1 || rootIdx > n) return
+
+        let count = 2 * len - 1
+        for(var i = 0; i < count; i++) {
+            if (i < len) {
+                if (!obj[rootIdx + i]) { obj[rootIdx + i] = [i + init]} 
+                else { obj[rootIdx + i].push(i + init)}
+            } else {
+                obj[rootIdx + len - 1].push(i + init)
+            }
+        }
+
+        runnable(len - 3, rootIdx + 2, init + 3 * (len - 1))
+
+        for(var i = 1; i <= len - 2; i++) {
+            obj[rootIdx + len - 1 - i].push(init + count - 1 + i)
+        }
+    }
+
+    runnable(n, 1, 1)
+
+    Object.keys(obj).map(key => obj[key].map(a => answer.push(a)))
+
+    return answer;
+}
+
+function make_list(n) {
+   return Array.from({length: n}).map((_,i) => 0)
 }
