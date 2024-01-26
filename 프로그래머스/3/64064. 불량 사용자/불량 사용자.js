8adb14137f0be9b50061237f2bbdf77ede1ec981
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-27 00:59:08 +0900

    [level 3] Title: 불량 사용자, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,34 +1,47 @@
-function solution(user_id, banned_id) {
-    const arr = [];
-    
-    banned_id.forEach((target)=>{
-        const possibleId = user_id.filter(username=>{
-            if (username.length !== target.length) return false
-            
-            for(let i=0; i<target.length; i++) {
-                if (target[i] === '*') continue
-                if (target[i] !== username[i]) return false
-            }
-            
-            return true
-        })
-        
-        arr.push(possibleId)
-    })
-    const answer = new Set()
-    dfs(0, arr.length, [], arr, answer);
-    return answer.size;
-}
-function dfs(n, targetN, temp, arr, answer) {
-    if (n===targetN) {
-        answer.add(temp.slice().sort().join(''))
-        return
+const isPossible = (bannedId, userId) => {
+    if (userId.length !== bannedId.length) return false;
+    for (let i = 0; i < bannedId.length; i++) {
+        if (bannedId[i] === '*' || bannedId[i] === userId[i]) continue;
+        return false
     }
-    
-    for(let i=0; i<arr[n].length; i++) {
-        if (temp.includes(arr[n][i])) continue;
-        temp.push(arr[n][i])
-        dfs(n+1, targetN, temp, arr,answer);
-        temp.pop()
+    return true;
+};
+
+const createIdInfos = (userIds, bannedIds, isPossible) => {
+    const result = new Array(bannedIds.length).fill(null).map(el => new Set());
+
+    for (const bIdx in bannedIds) {
+        for (const uIdx in userIds) {
+            if (!isPossible(bannedIds[bIdx], userIds[uIdx])) continue;
+            if (result[bIdx].has(uIdx)) continue;
+            result[bIdx].add(uIdx);
+        }
     }
-}
+    return result;
+};
+
+const getCount = (idInfos, bIdx = 0, idVisited = [], caseVisited = new Set()) => {
+    if (bIdx === idInfos.length) {
+        const visit = idVisited.reduce((acc, visited, i) => {
+            if (!visited) return acc;
+            return acc + i;
+        }, '');
+        if (caseVisited.has(visit)) return 0;
+        caseVisited.add(visit);
+        return 1;//visit.length === idInfos.length ? 1 : 0;
+    }
+
+    let result = 0;
+    for (const uIdx of idInfos[bIdx]) {
+        if (idVisited[uIdx]) continue;
+        idVisited[uIdx] = true;
+        result += getCount(idInfos, bIdx + 1, idVisited, caseVisited);
+        idVisited[uIdx] = false;
+    }
+    return result;
+};
+
+const solution = (userIds, bannedIds) => {
+    const idInfos = createIdInfos(userIds, bannedIds, isPossible);
+    return getCount(idInfos);
+};
