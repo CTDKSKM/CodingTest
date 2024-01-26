060fc71ebf4749ff25a895176bee1677383b2fe2
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-27 01:07:05 +0900

    [level 3] Title: 불량 사용자, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,35 +1,34 @@
-function solution(user_id, banned_id) {
-    const arr = [];
+const isUsed = {}
+const usedSets = new Set();
+const current = [];
 
-    banned_id.forEach((target)=>{
-        const possibleId = user_id.filter(username=>{
-            if (username.length !== target.length) return false
+function solution(user_id, banned_id, bannedIdx = 0) {
+    const bannedLen = banned_id.length;
+    if (bannedIdx === bannedLen) {
+        const sorted = current.slice().sort().toString();
+        if (usedSets.has(sorted)) {
+            return 0;
+        }
 
-            for(let i=0; i<target.length; i++) {
-                if (target[i] === '*' || target[i] === username[i]) continue
-                
-                return false
-            }
-
-            return true
-        })
-        arr.push(possibleId)
-    })
-
-    const answer = new Set()
-    dfs(0, arr.length, [], arr, answer);
-    return answer.size;
-}
-function dfs(n, targetN, temp, arr, answer) {
-    if (n===targetN) {
-        answer.add(temp.slice().sort().join(''))
-        return
+        usedSets.add(sorted);
+        return 1;
     }
 
-    for(let i=0; i<arr[n].length; i++) {
-        if (temp.includes(arr[n][i])) continue;
-        temp.push(arr[n][i])
-        dfs(n+1, targetN, temp, arr,answer);
-        temp.pop()
+    const regex = new RegExp(banned_id[bannedIdx].replace(/\*/g, '.'));
+    const matches = user_id.filter(user => {
+        const result = regex.exec(user);
+        return result && result[0].length === user.length;
+    });
+
+    let ret = 0;
+    for (const user of matches) {
+        if (!isUsed[user]) {
+            isUsed[user] = true;
+            current[bannedIdx] = user;
+            ret += solution(user_id, banned_id, bannedIdx + 1);
+            isUsed[user] = false;
+        }
     }
+
+    return ret;
 }
