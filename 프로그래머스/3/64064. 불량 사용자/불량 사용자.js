c9f110c3edc7e228a8a4e27966108ad0ce1d20fc
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-27 01:35:39 +0900

    [level 3] Title: 불량 사용자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,35 +1,35 @@
 function solution(user_id, banned_id) {
     const arr = [];
-
+    
     banned_id.forEach((target)=>{
         const possibleId = user_id.filter(username=>{
             if (username.length !== target.length) return false
 
             for(let i=0; i<target.length; i++) {
                 if (target[i] === '*' || target[i] === username[i]) continue
-                
                 return false
             }
-
+            
             return true
         })
         arr.push(possibleId)
-    })
 
+    })
+    
     const answer = new Set()
-    dfs(0, arr.length, [], arr, answer);
-    return answer.size;
+    dfs(0, arr.length, [], arr, answer)
+    return answer.size
 }
 function dfs(n, targetN, temp, arr, answer) {
     if (n===targetN) {
-        answer.add(temp.slice().sort().join(''))
+        answer.add(`${temp.slice().sort()}`)
         return
     }
-
+    
     for(let i=0; i<arr[n].length; i++) {
-        if (temp.includes(arr[n][i])) continue;
+        if (temp.includes(arr[n][i])) continue
         temp.push(arr[n][i])
-        dfs(n+1, targetN, temp, arr,answer);
+        dfs(n+1, targetN, temp, arr, answer)
         temp.pop()
     }
 }
