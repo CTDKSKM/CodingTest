299e3ae62b8616e636d010895f4629a6134d2573
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-27 01:23:09 +0900

    [level 3] Title: 불량 사용자, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,34 +1,45 @@
 function solution(user_id, banned_id) {
-    const candidates = [];
-    const answer = {};
-    banned_id.forEach(ban => {
-        const candidate = [];
-        user_id.forEach((id, i) => {
-            if (comp(ban, id)) candidate.push(i);
-        });
-        candidates.push(candidate);
-    });
+    let answer = 0;
+    let set = new Set();
+    const check = Array(user_id.length).fill(false);
 
-    function f(i = 0, selected = []) {
-        if (!candidates[i]) {
-            selected.sort();
-            answer[selected.join('')] = true;
+    function dfs(index, result, user_id, banned_id) {
+        if(index === banned_id.length) {
+            result.sort((a, b) => a-b);
+            set.add(result.join(''));
             return;
         }
-        candidates[i].filter(e => !selected.includes(e)).forEach(e => {
-            f(i + 1, selected.concat([e]));
-        });
-    }
-    f();
-    return Object.keys(answer).length;
-}
 
-function comp(ban, id) {
-    if (id.length !== ban.length) return false;
+        const banned_name = banned_id[index];
+        const blen = banned_name.length;
+        for(let i=0; i<user_id.length; i++) {
+            const user_name = user_id[i];
+            const ulen = user_name.length;
 
-    for (let i = 0; i < ban.length; i++) {
-        if (ban[i] === '*') continue;
-        if (ban[i] !== id[i]) return false;
+            if(blen !== ulen) continue;
+            if(check[i])  continue;
+
+            let flag = true;
+            for(let j=0; j<blen; j++) {
+                if(banned_name[j] === '*') continue;
+
+                if(user_name[j] !== banned_name[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag) {
+                check[i] = true;
+                const newResult = JSON.parse(JSON.stringify(result));
+                newResult.push(i);
+                dfs(index+1, newResult, user_id, banned_id);
+                check[i] = false;
+            }
+        }
     }
-    return true;
+
+    dfs(0, [], user_id, banned_id);
+    answer = set.size;
+    return answer;
 }
