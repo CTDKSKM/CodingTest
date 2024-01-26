bc54c2e2e1618915b1e8a2f8149e3aa7f815f134
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-01-27 01:23:20 +0900

    [level 3] Title: 불량 사용자, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,45 +1,35 @@
 function solution(user_id, banned_id) {
-    let answer = 0;
-    let set = new Set();
-    const check = Array(user_id.length).fill(false);
+    const arr = [];
 
-    function dfs(index, result, user_id, banned_id) {
-        if(index === banned_id.length) {
-            result.sort((a, b) => a-b);
-            set.add(result.join(''));
-            return;
-        }
+    banned_id.forEach((target)=>{
+        const possibleId = user_id.filter(username=>{
+            if (username.length !== target.length) return false
 
-        const banned_name = banned_id[index];
-        const blen = banned_name.length;
-        for(let i=0; i<user_id.length; i++) {
-            const user_name = user_id[i];
-            const ulen = user_name.length;
-
-            if(blen !== ulen) continue;
-            if(check[i])  continue;
-
-            let flag = true;
-            for(let j=0; j<blen; j++) {
-                if(banned_name[j] === '*') continue;
-
-                if(user_name[j] !== banned_name[j]) {
-                    flag = false;
-                    break;
-                }
+            for(let i=0; i<target.length; i++) {
+                if (target[i] === '*' || target[i] === username[i]) continue
+                
+                return false
             }
 
-            if(flag) {
-                check[i] = true;
-                const newResult = JSON.parse(JSON.stringify(result));
-                newResult.push(i);
-                dfs(index+1, newResult, user_id, banned_id);
-                check[i] = false;
-            }
-        }
+            return true
+        })
+        arr.push(possibleId)
+    })
+
+    const answer = new Set()
+    dfs(0, arr.length, [], arr, answer);
+    return answer.size;
+}
+function dfs(n, targetN, temp, arr, answer) {
+    if (n===targetN) {
+        answer.add(temp.slice().sort().join(''))
+        return
     }
 
-    dfs(0, [], user_id, banned_id);
-    answer = set.size;
-    return answer;
+    for(let i=0; i<arr[n].length; i++) {
+        if (temp.includes(arr[n][i])) continue;
+        temp.push(arr[n][i])
+        dfs(n+1, targetN, temp, arr,answer);
+        temp.pop()
+    }
 }
