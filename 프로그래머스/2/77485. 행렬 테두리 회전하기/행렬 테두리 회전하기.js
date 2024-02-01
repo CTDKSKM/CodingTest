3f693af4a36c636a572312924a9e2d1d45e22521
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-02-01 13:00:52 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 21.35 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,31 +1,38 @@
-const solution = (rows, columns, queries) => {
-  const rotate = (arr, query) => {
-    let [x1, y1, x2, y2] = query.map((item) => item - 1);
-    let beforeNum = arr[x1 + 1][y1];
-    let min = arr[x1 + 1][y1];
-    let x = x1;
-    let y = y1;
-    const total = (x2 - x1 + 1) * (y2 - y1 + 1) - (y2 - y1 - 1) * (x2 - x1 - 1);
-    for (let i = 0; i < total; i++) {
-      min = Math.min(arr[x][y], min); 
-      [beforeNum, arr[x][y]] = [arr[x][y], beforeNum];
-      if (y < y2 && x == x1) y++;
-      else if (y == y2 && x < x2) x++;
-      else if (x == x2 && y1 < y) y--;
-      else x--;
-    }
-    return min;
-  };
-  const ans = [];
-  const map = Array.from(Array(rows), () => Array(columns));
-  let num = 1;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      map[i][j] = num++;
-    }
-  }
-  for (let query of queries) {
-    ans.push(rotate(map, query));
-  }
-  return ans;
-};
+function solution(rows, columns, queries) {
+    const answer = [];
+    const direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    
+    let init = 1;
+    const arr = Array.from({length:rows}, ()=>Array.from({length:columns},()=>init++));
+
+    queries.forEach(val=>{
+        const [r1, c1, r2, c2] = val
+        let cnt = (c2-c1+1)*2 + (r2-r1-1)*2
+        let d_i = 0
+
+        let x = r1-1
+        let y = c1-1
+        const memo = arr[x][y]
+        let min = arr[x][y];
+
+        while(cnt) {
+            const [dx, dy] = direction[d_i%4]
+
+            if (x + dx >= r2 || y + dy >= c2 || x + dx < r1-1 || y + dy < c1-1) {d_i++; continue}
+
+            if (cnt === 1) arr[x][y] = memo
+            else arr[x][y] = arr[x+dx][y+dy]
+
+            x += dx
+            y += dy
+            
+            cnt--
+
+            min = Math.min(min, arr[x][y])
+        }
+        
+        answer.push(min)
+    })
+
+    return answer;
+}
