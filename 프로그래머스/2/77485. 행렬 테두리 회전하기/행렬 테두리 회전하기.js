9da9560dd3546b283a0d22f2733a716057171ba0
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-02-01 12:55:19 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 17.67 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,36 +1,43 @@
 function solution(rows, columns, queries) {
     const answer = [];
+    const direction = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    
     let init = 1;
     const arr = Array.from({length:rows}, ()=>Array.from({length:columns},()=>init++));
     
-    const direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-    
     queries.forEach(val=>{
-        const [r1, c1, r2, c2] = val
-        let cnt = (c2-c1+1)*2 + (r2-r1-1)*2
-        let d_i = 0
+        const [r1, c1, r2, c2] = val;
+        let cnt = (c2-c1+1)*2 + (r2-r1-1)*2;
+        let d_i = 0;
         
-        let x = r1-1
-        let y = c1-1
-        const memo = arr[x][y]
-        let min = memo;
+        let x = r1-1;
+        let y = c1-1;
+        const memo = arr[x][y];
+        let min = arr[x][y];
         
-        while(cnt) {
-            cnt--
+        while(cnt) {            
+            const [dx, dy] = direction[d_i%4];
             
-            const [dx, dy] = direction[d_i%4]
+            if (x + dx >= r2 || y + dy >= c2 || x + dx < r1-1 || y + dy < c1-1) {
+                d_i++; 
+                continue
+            }
             
-            if (x + dx >= r2 || y + dy >= c2 || x + dx < r1-1 || y + dy < c1-1) {d_i++; cnt++; continue}
+            if (cnt === 1) {
+                arr[x][y] = memo;
+            }
+            else {
+                arr[x][y] = arr[x+dx][y+dy];
+            }
             
-            if (cnt) arr[x][y] = arr[x+dx][y+dy]
-            else arr[x][y] = memo
+            x += dx;
+            y += dy;
             
-            x += dx
-            y += dy
+            cnt--;
             
-            min = Math.min(min, arr[x][y])
+            min = Math.min(min, arr[x][y]);
         }
-        answer.push(min)
+        answer.push(min);
     })
 
     return answer;
