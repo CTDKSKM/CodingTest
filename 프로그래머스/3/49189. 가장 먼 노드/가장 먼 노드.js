d4eb7f8a681979560962b19cecb556ebaeb43d2a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-03-11 16:01:52 +0900

    [level 3] Title: 가장 먼 노드, Time: 439.02 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,68 +1,26 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.links = [];
-    this.depth = null;
-  }
-
-  setLink(node) {
-    this.links.push(node);
-  }
-}
-
-class Graph {
-  constructor(n, edges) {
-    this.nodes = [];
-    for (let i = 1; i <= n; i++) {
-      this.nodes.push(new Node(i));
-    }
-    edges.forEach((edge) => {
-      this.nodes[edge[0] - 1].links.push(this.nodes[edge[1] - 1]);
-      this.nodes[edge[1] - 1].links.push(this.nodes[edge[0] - 1]);
-    });
-
-    this.queue = [];
-    this.numOfdeepestNodes = 0;
-
-    // console.log(this.nodes);
-  }
-
-  dfs(node) {
-    this.queue.push(node);
-    node.depth = 0;
-    let numOfdeepestNodes = 1;
-    let deepestDepth = 0;
-
-    while (this.queue.length !== 0) {
-      const poppedNode = this.queue.shift();
-      for (let i = 0; i < poppedNode.links.length; i++) {
-        // let numOfdeepestNodes = 1;
-        const linkedNode = poppedNode.links[i];
-        if (linkedNode.depth === null) {
-          this.queue.push(linkedNode);
-          linkedNode.depth = poppedNode.depth + 1;
-          if (deepestDepth < linkedNode.depth) {
-            deepestDepth = linkedNode.depth;
-            numOfdeepestNodes = 1;
-            // console.log('deepest init');
-          } else if (deepestDepth === linkedNode.depth) {
-            numOfdeepestNodes++;
-          }
-        }
-      }
-    }
-    this.numOfdeepestNodes = numOfdeepestNodes;
-    // console.log('dfs end,   deepestDepth', this.deepestDepth);
-  }
-
-  getNumOfDeepestNodes() {
-    this.dfs(this.nodes[0]);
-
-    return this.numOfdeepestNodes;
-  }
-}
-
 function solution(n, edge) {
-  const graph = new Graph(n, edge);
-  return graph.getNumOfDeepestNodes();
+    const graph = {}
+    edge.forEach(([a, b]) => {
+        graph[a] = (graph[a] || []).concat({node:b})
+        graph[b] = (graph[b] || []).concat({node:a})
+    })
+    
+    const dp = Array(n+1).fill(50000)
+    const queue = [{node:1, dist:0}]
+    let max = 0;
+    while (queue.length) {
+        const {node, dist} = queue.shift()
+
+        graph[node].forEach(({node:next})=>{
+            if (dp[next] > dist+1) {
+                dp[next] = dist+1
+                queue.push({node:next, dist:dp[next]})
+                max = Math.max(max, dp[next])
+            }
+        })
+
+    }
+    dp[1] = 0
+
+    return dp.filter(v=>v===max).length;
 }
