e4b39029575415a5a393e5a8f7d0ff8bed04150a
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-03-11 15:51:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.29 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,30 @@
+function solution(n, edge) {
+    const graph = {}
+    edge.forEach(([a, b]) => {
+        graph[a] = graph[a] || []
+        graph[b] = graph[b] || []
+        graph[a].push({node:b})
+        graph[b].push({node:a})
+    })
+    const dp = Array(n+1).fill(50000)
+    const queue = [{node:1, dist:0}]
+    let max = 0;
+    while (queue.length) {
+        const {node, dist} = queue.shift()
+
+        graph[node].forEach(({node:next})=>{
+            if (dp[next] > dist+1) {
+                dp[next] = dist+1
+                queue.push({node:next, dist:dp[next]})
+                max = Math.max(max, dp[next])
+            }
+        })
+
+    }
+    dp[1] = 0
+    
+    return dp.filter(v=>v===max).length;
+}
+/*
+
+*/
