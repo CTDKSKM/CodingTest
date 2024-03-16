6f76c875b297614986dcf8756a5e3923532f7321
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-03-16 23:27:35 +0900

    [level 3] Title: 합승 택시 요금, Time: 6.91 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,15 +1,44 @@
 function solution(n, s, a, b, fares) {
-    const min = (a, b) => a < b ? a : b, inf = 100000000;
-    let d = Array(n).fill(0).map(() => Array(n).fill(inf)), l;
-    for (const [t, f, v] of fares)
-        d[t - 1][f - 1] = d[f - 1][t - 1] = v;
-    for (let t = 0; t < n; t++)
-        d[t][t] = 0;
-    for (let k = 0; k < n; k++)
-        for (let i = 0; i < n; i++)
-            for (let j = 0; j < n; j++)
-                if (d[i][j] > (l = d[i][k] + d[k][j]))
-                    d[i][j] = l;
-    s--; a--; b--;
-    return [...Array(n).keys()].reduce((m, k) => min(m, d[s][k] + d[k][a] + d[k][b]), inf);
+    var answer = 0;
+
+    const map = Array(n + 1).fill(null).map(() => Array(n + 1).fill(Infinity));
+
+    fares.forEach(([start, end, dist]) => {
+        map[start][end] = map[end][start] = dist;
+    });
+
+    const distFromA = findDistToAllNodes(n, map, a);
+    const distFromB = findDistToAllNodes(n, map, b);
+    const distFromS = findDistToAllNodes(n, map, s);
+
+    return Math.min(...distFromS.map((dist, idx) => dist + distFromA[idx] + distFromB[idx]));
+}
+
+function findDistToAllNodes(n, map, node) {
+    const dist = Array(n + 1).fill(Infinity);
+    const visit = Array(n + 1).fill(false);
+    let lastVisit = node;
+
+    dist[node] = 0;
+    visit[node] = true;
+
+    for (let i = 0; i < n - 1; i++) {
+        map[lastVisit].forEach((d, idx) => {
+            dist[idx] = Math.min(d + dist[lastVisit], dist[idx]);
+        });
+
+        let min = Infinity;
+
+        for (let j = 1; j <= n; j++) {
+            if (visit[j]) continue;
+
+            if (dist[j] < min) {
+                lastVisit = j;
+                min = dist[j];
+            }
+        }
+        visit[lastVisit] = true;
+    }
+
+    return dist;
 }
