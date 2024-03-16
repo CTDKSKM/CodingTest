50ed4e3aa24e37ff9a8048cd4075b635c221f69f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-03-16 23:49:41 +0900

    [level 3] Title: 합승 택시 요금, Time: 11.91 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,38 +1,29 @@
 function solution(n, s, a, b, fares) {
-    const map = Array(n + 1).fill(null).map(() => Array(n + 1).fill(Infinity));
-    fares.forEach(([start, end, dist]) => {
-        map[start][end] = map[end][start] = dist;
-    });
-    const distFromA = findDistToAllNodes(n, map, a);
-    const distFromB = findDistToAllNodes(n, map, b);
-    const distFromS = findDistToAllNodes(n, map, s);
-    return Math.min(...distFromS.map((dist, idx) => dist + distFromA[idx] + distFromB[idx]));
-}
+    let ret = 20000000;
 
-function findDistToAllNodes(n, map, node) {
-    const dist = Array(n + 1).fill(Infinity);
-    const visit = Array(n + 1).fill(false);
-    let lastVisit = node;
+    let graph = new Array(n+1).fill(0).map(e=>new Array(n+1).fill(1000000));
 
-    dist[node] = 0;
-    visit[node] = true;
-
-    for (let i = 0; i < n - 1; i++) {
-        map[lastVisit].forEach((d, idx) => {
-            dist[idx] = Math.min(d + dist[lastVisit], dist[idx]);
-        });
-        let min = Infinity;
-
-        for (let j = 1; j <= n; j++) {
-            if (visit[j]) continue;
-
-            if (dist[j] < min) {
-                lastVisit = j;
-                min = dist[j];
-            }
-        }
-        visit[lastVisit] = true;
+    for (let fare of fares) {
+        graph[fare[0]][fare[1]] = fare[2];
+        graph[fare[1]][fare[0]] = fare[2];
     }
 
-    return dist;
+    for(let i=1; i<=n; i++) {
+        graph[i][i] = 0;
+    }
+
+    for(let i=1; i<=n; i++) {
+        for(let j=1; j<=n; j++ ) {
+            for(let k=1; k<=n; k++) {
+                graph[j][k] = Math.min(graph[j][k],graph[j][i]+graph[i][k])
+            }
+        }
+    }
+
+    for(let i=1; i<=n; i++) {
+        ret = Math.min(ret,graph[s][i]+graph[i][a]+graph[i][b]);
+    }
+
+    return ret;
 }
+
