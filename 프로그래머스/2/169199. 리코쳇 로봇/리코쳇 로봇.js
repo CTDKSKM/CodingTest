dbe4028df249adc0842f1aaa5a57c2f9bfab2a1e
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-03-31 22:21:57 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.52 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,105 +1,61 @@
-function findNear(arr, value, options, limit) {
-    if (options === 'less') {
-        for (let i = arr.length - 1; i >= 0; i--) {
-            if(arr[i] < value) {
-                return arr[i] + 1
-            }
-        }
-        return 0
-    }
+const direction = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
 
-    if (options === 'grater') {
-        for (let i = 0; i < arr.length; i++) {
-            if(arr[i] > value) {
-                return arr[i] - 1
-            }
-        }
-
-        return limit - 1
-    }
-}
-
+let n, m;
 function solution(board) {
-    let n = board.length
-    let m = board[0].length
-
-    let visited = new Array(n).fill(0).map(() => new Array(m).fill(0).map(() => Infinity))
-    let start, goal;
-
-    let disturbanceRow = {}
-    let disturbanceCol = {}
-
-    for (let i = 0; i < n; i++) {
-        disturbanceRow[i] = []
-    }
-
+  n = board.length;
+  m = board[0].length;
+  let sx, sy, ex, ey;
+  for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-        disturbanceCol[j] = []
+      if (board[i][j] === "R") {
+        sx = i;
+        sy = j;
+      }
+      if (board[i][j] === "G") {
+        ex = i;
+        ey = j;
+      }
     }
+  }
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (board[i][j] === 'R') start = [i, j]
-            if (board[i][j] === "G") goal = [i, j]
-            if (board[i][j] === 'D') {
-                disturbanceRow[i].push(j)
-                disturbanceCol[j].push(i)
-            }
+  const discovered = Array.from(Array(n), () => Array(m).fill(false));
+  discovered[sx][sy] = true;
+  let queue = [[sx, sy, 0]];
+  while (queue.length !== 0) {
+    const size = queue.length;
+    let nextQueue = [];
+
+    for (let i = 0; i < size; i++) {
+      const [x, y, cnt] = queue.pop();
+
+      for (const [dx, dy] of direction) {
+        let nx = x + dx;
+        let ny = y + dy;
+        while (check(nx, ny) && board[nx][ny] !== "D") {
+          nx += dx;
+          ny += dy;
         }
+        nx -= dx;
+        ny -= dy;
+        if (board[nx][ny] === "G") {
+          return cnt + 1;
+        }
+        if (!discovered[nx][ny]) {
+          discovered[nx][ny] = true;
+          nextQueue.push([nx, ny, cnt + 1]);
+        }
+      }
     }
-
-    let queue = [start]
-    visited[start[0]][start[1]] = 0
-
-    while (queue.length > 0) {
-        const [row, col] = queue.shift()
-        let nRow, nCol;
-
-        // 왼쪽으로 쭉
-        nRow = row
-        nCol = findNear(disturbanceRow[row], col, 'less', m)
-        if (0 <= nRow && nRow < n && 0 <= nCol && nCol < m) {
-            if (board[nRow][nCol] === "D") continue
-            if (visited[nRow][nCol] > visited[row][col] + 1) {
-                visited[nRow][nCol] = visited[row][col] + 1
-                queue.push([nRow, nCol])
-            }
-        }
-
-        // 오른쪽으로 쭉
-        nRow = row 
-        nCol = findNear(disturbanceRow[row], col, 'grater', m)
-        if (0 <= nRow && nRow < n && 0 <= nCol && nCol < m) {
-            if (visited[nRow][nCol] > visited[row][col] + 1) {
-                visited[nRow][nCol] = visited[row][col] + 1
-                queue.push([nRow, nCol])
-            }
-        }
-
-        // 위쪽으로 쭉
-        nRow = findNear(disturbanceCol[col], row, 'less', n)
-        nCol = col
-        if (0 <= nRow && nRow < n && 0 <= nCol && nCol < m) {
-            if (visited[nRow][nCol] > visited[row][col] + 1) {
-                visited[nRow][nCol] = visited[row][col] + 1
-                queue.push([nRow, nCol])
-            }
-        }
-
-        // 아래쪽으로 쭉
-        nRow = findNear(disturbanceCol[col], row, 'grater', n)
-        nCol = col
-        if (0 <= nRow && nRow < n && 0 <= nCol && nCol < m) {
-            if (visited[nRow][nCol] > visited[row][col] + 1) {
-                visited[nRow][nCol] = visited[row][col] + 1
-                queue.push([nRow, nCol])
-            }
-        }
-    }
-
-
-
-    const result = visited[goal[0]][goal[1]]
-
-    return result === Infinity ? -1 : result
+    queue = nextQueue;
+  }
+  return -1;
 }
+
+const check = (nx, ny) => {
+  return 0 <= nx && nx < n && 0 <= ny && ny < m;
+};
