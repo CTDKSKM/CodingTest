213b6fae82aecab3e00da4117e551d07c8debbbb
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-04-14 02:01:13 +0900

    [level 3] Title: 섬 연결하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,15 +1,27 @@
-function solution(n, costs) {
-    costs.sort((a,b) => a[2] - b[2]);
-    let [from, to, answer] = costs.shift();
-    let connected = new Set([from, to]);
-    while (connected.size < n) {
-        let index = costs.findIndex(([from, to]) =>
-                                    connected.has(from) && !connected.has(to)
-                                    || connected.has(to) && !connected.has(from)
-                                   );
-        let [[from, to, cost]] = costs.splice(index, 1);
-        answer += cost;
-        connected.add(from).add(to);
+const getParent = (parent, c) => {
+  if (parent[c] === c) return c;
+  return (parent[c] = getParent(parent, parent[c]));
+};
+const unionParent = (parent, c1, c2) => {
+  const [n1, n2] = [getParent(parent, c1), getParent(parent, c2)];
+  if (n1 < n2) return (parent[n2] = n1);
+  else return (parent[n1] = n2);
+};
+const findParent = (parent, c1, c2) => {
+  const [n1, n2] = [getParent(parent, c1), getParent(parent, c2)];
+  return n1 === n2;
+};
+
+const solution = (n, costs) => {
+  let answer = 0;
+  const parent = new Array(n).fill(null).map((_, i) => i);
+  costs.sort((a, b) => a[2] - b[2]);
+
+  for (const cost of costs) {
+    if (!findParent(parent, cost[0], cost[1])) {
+      answer += cost[2];
+      unionParent(parent, cost[0], cost[1]);
     }
-    return answer;
-}
+  }
+  return answer;
+};
