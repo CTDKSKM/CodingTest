8733f88da9fd04f36e660e9bbf309d42857b5c06
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-04-22 12:13:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,22 +1,54 @@
 function solution(jobs) {
-    jobs.sort(([a, b], [c, d]) => a - c || b - d);
-    const waiting = [];
-    const count = jobs.length;
-    let processed_time = 0;
-    let time = 0;
-    while (jobs.length + waiting.length) {
-        let in_process;
-        while (jobs.length && (jobs[0][0] <= time)) {
-            waiting.push(jobs[0] && jobs.shift());
+    let total = jobs.length
+    let answer = 0
+    let tick =0
+    let queue=[]
+    while(true){
+        // 1. (jobs가 있을 때) tick보다 같거나 작은 작업 모두 추출 후 queue에 삽입
+        let index = 0;
+        while(true){
+            if(index >= jobs.length) break;
+            if(jobs[index][0] <= tick){
+            // 요청이 들어온 작업
+                queue.push(jobs.splice(index,1)[0])
+            }else{
+            // 요청 전 작업
+                index++;
+            }
         }
-        if (waiting.length) {
-            in_process = waiting.sort(([a, b], [c, d]) => b - d || a - c).shift();
-        } else {
-            in_process = jobs.shift();
-            time = in_process[0];
+
+
+        // 3. queue와 jobs가 0이면 끝
+        if(queue.length===0 ){
+             if(jobs.length ===0){
+                 break;
+             }else{
+                tick++;
+             }
+        }else{
+             // 2. (queue가 있을 때) queue에서 작업량이 가장 작은 작업 수행
+        let minIndex = 0;
+        for(let i = 1 ; i < queue.length ; i++){
+            if(queue[minIndex][1] > queue[i][1]) minIndex=i
         }
-        time += in_process[1];
-        processed_time += time - in_process[0];
+
+        // 2.1 수행한 시간 만큼 tick 증가
+        tick += queue[minIndex][1]
+
+        // 2.2 answer += tick - 수행한 작업의 작업 처리 요청 시간 
+        answer += tick - queue[minIndex][0]
+
+        // 2.3 queue에서 해당 job 삭제
+        queue.splice(minIndex,1)
+
+        }
+
+
+
+
+
     }
-    return Math.floor(processed_time / count);
+
+    return Math.floor(answer/total)
+
 }
