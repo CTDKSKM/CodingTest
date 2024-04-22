28c04fde49b938d1ee9174a927dcc1af561b4da5
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-04-22 21:48:16 +0900

    [Gold III] Title: Thread Knots, Time: 464 ms, Memory: 45780 KB -BaekjoonHub

---
+++ 백준/Gold/17976. Thread Knots/Thread Knots.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
+
+const n = parseInt(input[0]);
+
+const stack = [];
+for (let i = 1; i <= n; i++) {
+  stack.push(input[i].split(" ").map(Number));
+}
+stack.sort((a, b) => a[0] - b[0]);
+
+let l = 0;
+let r = stack[n - 1].reduce((a, b) => a + b);
+
+let ans = 0;
+while (l <= r) {
+  let mid = Math.floor((l + r) / 2);
+  let cur = stack[0][0];
+  let cnt = n - 1;
+  for (let i = 1; i < n; i++) {
+    const [min, alpha] = stack[i];
+
+    if (cur + mid > min + alpha) break;
+    else cur = Math.max(cur + mid, min);
+    cnt--;
+  }
+
+  if (cnt) r = mid - 1;
+  else {
+    l = mid + 1;
+    ans = Math.max(ans, mid);
+  }
+}
+
+console.log(ans);
