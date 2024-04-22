332e175cfa5c0b9fc07307c11cea1e9d4caa6368
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-04-22 12:19:39 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,23 +1,84 @@
-function solution(jobArr) {
-    var answer = 0;
-    jobArr = jobArr.sort((a, b) => a[0] - b[0]);
-    let queue = [];
-    let time = 0;
-    let i = 0;
-    while(queue.length > 0 || i < jobArr.length) {
-        if(jobArr.length > i && jobArr[i][0] <= time) {
-            queue.push(jobArr[i++]);
-            queue.sort((a, b) => a[1] - b[1]);
-            continue;
-        }
-        if(queue.length > 0) {
-            let [ start, work ] = queue.shift();
-            answer = answer + time - start + work;
-            time += work;
-        } else if(queue.length == 0) {
-            time = jobArr[i][0];
-        }
-    }
+class Heap {
+  constructor() {
+    this.heap = [];
+  }
 
-    return parseInt(answer / jobArr.length);
+  getSize() {
+    return this.heap.length;
+  }
+  getLeftIdx(node) {
+    return node * 2 + 1;
+  }
+  getRightIdx(node) {
+    return node * 2 + 2;
+  }
+  getParentIdx(node) {
+    return parseInt((node - 1) / 2);
+  }
+  swap(x, y) {
+    [this.heap[x], this.heap[y]] = [this.heap[y], this.heap[x]];
+  }
+  bubbleUp() {
+    let target = this.getSize() - 1;
+    let parentIdx = this.getParentIdx(target);
+    while (this.heap[target][1] < this.heap[parentIdx][1]) {
+      this.swap(target, parentIdx);
+      target = parentIdx;
+      parentIdx = this.getParentIdx(target);
+    }
+  }
+  bubbleDown() {
+    let target = 0;
+    let leftIdx = this.getLeftIdx(target);
+    let rightIdx = this.getRightIdx(target);
+    while (
+      (this.heap[leftIdx] && this.heap[target][1] > this.heap[leftIdx][1]) ||
+      (this.heap[rightIdx] && this.heap[target][1] > this.heap[rightIdx][1])
+    ) {
+      let small = leftIdx;
+      if (this.heap[rightIdx] && this.heap[small][1] > this.heap[rightIdx][1]) {
+        small = rightIdx;
+      }
+      this.swap(target, small);
+      target = small;
+      leftIdx = this.getLeftIdx(target);
+      rightIdx = this.getRightIdx(target);
+    }
+  }
+
+  pushItem(node) {
+    this.heap.push(node);
+    this.bubbleUp();
+  }
+
+  shiftItem() {
+    if (this.getSize() === 0) return null;
+    if (this.getSize() === 1) return this.heap.pop();
+    let temp = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return temp;
+  }
+}
+function solution(jobs) {
+  const heap = new Heap();
+  jobs = jobs.sort((a, b) => (a[0] - b[0] === 0 ? a[1] - b[1] : a[0] - b[0]));
+  let totalJobs = jobs.length;
+  let done = 0;
+  let temp = [];
+  let current = 0;
+  while (done < totalJobs) {
+    while (jobs[0] && current >= jobs[0][0]) {
+      heap.pushItem(jobs.shift());
+    }
+    if (heap.getSize()) {
+      let [startTime, cost] = heap.shiftItem();
+      temp.push(current + cost - startTime);
+      current += cost;
+      done++;
+    } else {
+      current++;
+    }
+  }
+  return parseInt(temp.reduce((a, b) => a + b) / temp.length);
 }
