ca369579de3808e8e7cf808eebc2c748bdbef4da
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-04-22 12:15:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,54 +1,23 @@
-function solution(jobs) {
-    let total = jobs.length
-    let answer = 0
-    let tick =0
-    let queue=[]
-    while(true){
-        // 1. (jobs가 있을 때) tick보다 같거나 작은 작업 모두 추출 후 queue에 삽입
-        let index = 0;
-        while(true){
-            if(index >= jobs.length) break;
-            if(jobs[index][0] <= tick){
-            // 요청이 들어온 작업
-                queue.push(jobs.splice(index,1)[0])
-            }else{
-            // 요청 전 작업
-                index++;
-            }
+function solution(jobArr) {
+    var answer = 0;
+    jobArr = jobArr.sort((a, b) => a[0] - b[0]);
+    let queue = [];
+    let time = 0;
+    let i = 0;
+    while(queue.length > 0 || i < jobArr.length) {
+        if(jobArr.length > i && jobArr[i][0] <= time) {
+            queue.push(jobArr[i++]);
+            queue.sort((a, b) => a[1] - b[1]);
+            continue;
         }
-
-
-        // 3. queue와 jobs가 0이면 끝
-        if(queue.length===0 ){
-             if(jobs.length ===0){
-                 break;
-             }else{
-                tick++;
-             }
-        }else{
-             // 2. (queue가 있을 때) queue에서 작업량이 가장 작은 작업 수행
-        let minIndex = 0;
-        for(let i = 1 ; i < queue.length ; i++){
-            if(queue[minIndex][1] > queue[i][1]) minIndex=i
+        if(queue.length > 0) {
+            let [ start, work ] = queue.shift();
+            answer = answer + time - start + work;
+            time += work;
+        } else if(queue.length == 0) {
+            time = jobArr[i][0];
         }
-
-        // 2.1 수행한 시간 만큼 tick 증가
-        tick += queue[minIndex][1]
-
-        // 2.2 answer += tick - 수행한 작업의 작업 처리 요청 시간 
-        answer += tick - queue[minIndex][0]
-
-        // 2.3 queue에서 해당 job 삭제
-        queue.splice(minIndex,1)
-
-        }
-
-
-
-
-
     }
 
-    return Math.floor(answer/total)
-
+    return parseInt(answer / jobArr.length);
 }
