08f1a588f1dfe78a73fdf749eade65db9c2401d4
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-05-17 12:34:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 10.28 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,37 +1,15 @@
 function solution(n, s, a, b, fares) {
-    const graph = {};
-    fares.forEach(([from, to, fare])=>{
-        graph[from] = graph[from] || [];
-        graph[to] = graph[to] || [];
-        graph[from].push({node:to, fare})
-        graph[to].push({node:from, fare})
-    })
-
-    //S에서 각 지점으로 가는 최소 비용 구하기.
-    const fromS = calc(s, 0, graph, n)
-
-    //각 지점에서 A, B로 가는 최소비용 구하기
-    let ret = Infinity
-    fromS.map((fare,idx)=>({node:idx, fare})).filter(v=>v.fare !== Infinity).forEach(({node, fare})=>{
-        const toDest = calc(node, fare, graph, n)
-
-        ret = Math.min(ret, toDest[a]+toDest[b]-fare)
-    })
-    
-    return ret
-}
-function calc(node, fare, graph, n) {
-    const dp = Array(n+1).fill(Infinity)
-    dp[node] = fare
-    const q = [{node, fare}]
-    while(q.length) {
-        const {node:from, fare:f_fare} = q.shift()
-        graph[from].forEach(({node:to, fare:t_fare})=>{
-            if (dp[to] > t_fare + f_fare) {
-                dp[to] = t_fare+f_fare
-                q.push({node:to, fare:dp[to]})
-            }
-        })
-    }
-    return dp
+    const min = (a, b) => a < b ? a : b, inf = 100000000;
+    let d = Array(n).fill(0).map(() => Array(n).fill(inf)), l;
+    for (const [t, f, v] of fares)
+        d[t - 1][f - 1] = d[f - 1][t - 1] = v;
+    for (let t = 0; t < n; t++)
+        d[t][t] = 0;
+    for (let k = 0; k < n; k++)
+        for (let i = 0; i < n; i++)
+            for (let j = 0; j < n; j++)
+                if (d[i][j] > (l = d[i][k] + d[k][j]))
+                    d[i][j] = l;
+    s--; a--; b--;
+    return [...Array(n).keys()].reduce((m, k) => min(m, d[s][k] + d[k][a] + d[k][b]), inf);
 }
