dbab30f2214bb3083dd72cb9aa803f4952478ba6
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-05-25 21:12:29 +0900

    [level 3] Title: 부대복귀, Time: 101.38 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,27 +1,24 @@
 function solution(n, roads, sources, destination) {
-    const graph = new Array(n+1).fill(null).map(_=>[]);
-    for(let [a,b] of roads){
-        graph[a].push(b);
-        graph[b].push(a);
+    const graph = Array.from({ length: n + 1 }, () => []);
+    for (const [x, y] of roads) {
+        graph[x].push(y);
+        graph[y].push(x);
     }
-    const visited = new Array(n+1).fill(Infinity);
-    const bfs = (destination) =>{
-        const q = [destination];
-        visited[destination] = 0;
-        while(q.length > 0){
-            const idx = q.shift();
-            for(let newIdx of graph[idx]){
-                if(visited[idx]+1 < visited[newIdx]){
-                    visited[newIdx] = visited[idx]+1;
-                    q.push(newIdx);
-                }
+
+    const dist = new Array(n + 1).fill(-1);
+    dist[destination] = 0;
+    const queue = [destination];
+
+    while (queue.length) {
+        const node = queue.shift();
+        const nextNodeDistance = dist[node] + 1;
+        for (const y of graph[node]) {
+            if (dist[y] === -1) {
+                dist[y] = nextNodeDistance;
+                queue.push(y);
             }
         }
     }
-    bfs(destination);
 
-    return sources.map(v=>{
-        if(visited[v] === Infinity) return -1;
-        else return visited[v];
-    });
+    return sources.map(i => dist[i]);
 }
