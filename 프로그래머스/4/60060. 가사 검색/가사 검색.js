b974ec4b345d31192bc26842350a1ebffbed00cd
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-02 22:24:56 +0900

    [level 4] Title: 가사 검색, Time: 78.36 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.js
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.js
@@ -1,30 +1,49 @@
-const reverseStr = str => str.split('').reverse().join('')
-function solution (words, queries) {
-    const lens = {}
-    const [pre, suf] = [words, words.map(reverseStr)].map((words, i) => {
-        const root = {}
-        words.forEach(w => {
-            let [o, len] = [root, w.length]
-            if (i === 0) lens[len] = (lens[len] || 0) + 1
-            do {
-                const node = o[w[0] + len] || (o[w[0] + len] = { cnt: 0, next: {} })
-                node.cnt++
-                o = node.next
-                w = w.slice(1)
-            } while (w)
-        })
-        return root
-    })
-    return queries.map(q => q[0] === '?' ? [suf, reverseStr(q)] : [pre, q]).map(([o, q]) => {
-        let len = q.length
-        if (q[0] === '?') return lens[len] || 0
-        let cnt = 0
-        do {
-            const node = o[q[0] + len]
-            if (!node) return 0
-            cnt = node.cnt
-            o = node.next
-        } while ((q = q.slice(1))[0] !== '?')
-        return cnt
-    })
+function counts(arr, t) {
+    const replace = (str, char) => str.replace(/\?/g, char);
+    const a = bisectLeft(arr, replace(t, 'a'));
+    const z = bisectRight(arr, replace(t, 'z'));
+    return z - a;
+}
+
+function bisectLeft(arr, x) {
+    let lo = 0, hi = arr.length;
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi) / 2);
+        if (arr[mid] < x) lo = mid + 1;
+        else hi = mid;
+    }
+    return lo;
+}
+
+function bisectRight(arr, x) {
+    let lo = 0, hi = arr.length;
+    while (lo < hi) {
+        const mid = Math.floor((lo + hi) / 2);
+        if (arr[mid] <= x) lo = mid + 1;
+        else hi = mid;
+    }
+    return lo;
+}
+
+function solution(words, queries) {
+    const result = [];
+    words.sort();
+    const wordsReverse = [...words].map(word => word.split('').reverse().join('')).sort();
+    const newWordsReverses = {};
+    const newWordss = {};
+
+    for (const q of queries) {
+        if (q[0] === '?') {
+            if (!newWordsReverses.hasOwnProperty(q.length)) {
+                newWordsReverses[q.length] = wordsReverse.filter(word => word.length === q.length);
+            }
+            result.push(counts(newWordsReverses[q.length], q.split('').reverse().join('')));
+        } else {
+            if (!newWordss.hasOwnProperty(q.length)) {
+                newWordss[q.length] = words.filter(word => word.length === q.length);
+            }
+            result.push(counts(newWordss[q.length], q));
+        }
+    }
+    return result;
 }
