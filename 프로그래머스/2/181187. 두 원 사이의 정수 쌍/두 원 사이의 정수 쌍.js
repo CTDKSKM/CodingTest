27d6db6a3d571075eaec2517d310a2e1995d073f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-05 14:24:26 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 13.40 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -1,11 +1,18 @@
 function solution(r1, r2) {
-    let cnt = 0;
-    for(let x=1; x<=r2; x++) {
-        const x2 = Math.pow(x, 2);
-        const largeY = Math.sqrt(Math.pow(r2, 2)-x2);
-        const smallY = Math.sqrt(Math.pow(r1, 2)-x2) || 0;
-        
-        cnt += Math.floor(largeY) - Math.ceil(smallY) + 1
+    const calculateMinH = (v) => {
+        if (v > r1) return 0
+        return Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(v, 2)))
     }
-    return cnt * 4;
+    const calculateMaxH = (v) => {
+        if (v > r2) return 0
+        return Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(v, 2)))
+    }
+    let temp = 0
+    for (let x = 0; x < r2; x++) {
+        const min = calculateMinH(x)
+        const max = calculateMaxH(x)
+        const noOfDots = min !== 0 ? max - min + 1 : max - min
+        temp += noOfDots
+    }
+    return temp * 4
 }
