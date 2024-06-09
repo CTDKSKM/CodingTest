c12cea297a26210c5819b7b4cf4058802c5185a3
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-09 21:57:16 +0900

    [level 2] Title: 과제 진행하기, Time: 2.87 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -1,24 +1,37 @@
 function solution(plans) {
-    let sortList = [];
-    for (let item of plans) {
-        let temp = item[1].split(":");
-        let times = parseInt(temp[0]) * 60 + parseInt(temp[1]);
-        item[1] = times;
-        sortList.push(item);
-    }
-    sortList.sort((a, b) => b[1] - a[1]);
+    const answer = [];
+    const stop = [];
 
-    let answer = [];
-    while (sortList.length) {
-        let temp = sortList.pop();
-        for (let index = 0; index < answer.length; index++) {
-            if (answer[index][0] > temp[1]) {
-                answer[index][0] += parseInt(temp[2]);
+    plans = plans.map(v => { v[1] = v[1].split(":").reduce((acc, cur) => 60*Number(acc) + Number(cur)); v[2] = Number(v[2]); return v; }).sort((a, b) => a[1] - b[1]);
+
+    let [curr, time, last] = plans[0];
+    let i = 1;
+    while(i < plans.length)
+    {
+        let next = plans[i];
+        if(last > next[1] - time)
+        {
+            last -= next[1] - time;
+            stop.push([curr, last]);
+        }
+        else
+        {
+            answer.push(curr);
+            if(stop.length > 0)
+            {
+                const tmp = stop.pop();
+                [curr, time, last] = [tmp[0], time+last, tmp[1]];
+                continue;
             }
         }
-        answer.push([temp[1] + parseInt(temp[2]), temp[0]]);
+
+        [curr, time, last] = next;
+        i++;
     }
-    answer.sort((a, b) => a[0] - b[0]);
-    
-    return answer.flatMap(item => item.filter(i => typeof i === 'string'));
+
+    answer.push(plans.pop()[0]);
+    while(stop.length > 0)
+        answer.push(stop.pop()[0]);
+
+    return answer;
 }
