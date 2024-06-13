5361cf3f32aa31e1664b4d09d18d4892e284ff1f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-13 10:55:27 +0900

    [level 3] Title: 순위, Time: 43.04 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,36 +1,62 @@
 function solution(n, results) {
-    // 승패 관계를 저장할 2차원 배열 초기화
-    const win = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
-    
-    // 승패 관계 설정
-    results.forEach(([winner, loser]) => {
-        win[winner][loser] = true;
-    });
-    // 플로이드-워셜 알고리즘
-    for (let k = 1; k <= n; k++) {
-        for (let i = 1; i <= n; i++) {
-            for (let j = 1; j <= n; j++) {
-                if (win[i][k] && win[k][j]) {
-                    win[i][j] = true;
+    // i가 이긴 사람, 진 사람을 리스트에 저장한다
+    const winList = Array.from({ length: n }, () => []);
+    const loseList = Array.from({ length: n }, () => []);
+
+    for (let i = 1; i <= n; i++) {
+        const win = [];
+        const lose = [];
+
+        for (let j = 0; j < results.length; j++) {
+            if (results[j][0] === i && !win.includes(results[j][1])) {
+                win.push(results[j][1]);
+            }
+            if (results[j][1] === i && !lose.includes(results[j][0])) {
+                lose.push(results[j][0]);
+            }
+        }
+
+        win.sort((a, b) => a - b);
+        lose.sort((a, b) => a - b);
+
+        winList[i - 1] = win;
+        loseList[i - 1] = lose;
+    }
+
+    // i가 이긴 사람 리스트에 이긴 사람에게 진 사람도 포함시켜준다
+    for (let i = 0; i < winList.length; i++) {
+        for (let j = 0; j < winList[i].length; j++) {
+            const winner = winList[i][j] - 1;
+            for (let k = 0; k < winList[winner].length; k++) {
+                const person = winList[winner][k];
+                if (!winList[i].includes(person)) {
+                    winList[i].push(person);
                 }
             }
         }
     }
-    
-    let answer = 0;
-    // 각 선수별로 확실한 승패 관계를 체크하여 순위를 확정할 수 있는 선수의 수를 계산
-    for (let i = 1; i <= n; i++) {
-        let count = 0;
-        for (let j = 1; j <= n; j++) {
-            if (win[i][j] || win[j][i]) {
-                count++;
+
+    // i가 진 사람 리스트에 진 사람을 이긴 사람도 포함시킨다
+    for (let i = 0; i < loseList.length; i++) {
+        for (let j = 0; j < loseList[i].length; j++) {
+            const loser = loseList[i][j] - 1;
+            for (let k = 0; k < loseList[loser].length; k++) {
+                const person = loseList[loser][k];
+                if (!loseList[i].includes(person)) {
+                    loseList[i].push(person);
+                }
             }
         }
-        if (count === n - 1) {
+    }
+
+    // 순위를 알 수 있는 사람은 이긴 사람 + 진 사람 합이 n-1인 경우다
+    let answer = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (winList[i].length + loseList[i].length === n - 1) {
             answer++;
         }
-        
     }
-    
+
     return answer;
 }
