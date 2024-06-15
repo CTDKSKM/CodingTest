53909281ef9af95c1ced66481dbbeb981f46d11c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-15 19:08:23 +0900

    [Silver I] Title: 알고리즘 수업 - 퀵 정렬 3, Time: 432 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Silver/24092. 알고리즘 수업 － 퀵 정렬 3/알고리즘 수업 － 퀵 정렬 3.js
+++ 백준/Silver/24092. 알고리즘 수업 － 퀵 정렬 3/알고리즘 수업 － 퀵 정렬 3.js
@@ -1,44 +1,48 @@
-const fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-function different() {
-  for (let i = 0; i < N; i++) if (A[i] !== B[i]) return i
-  return false
-} 
-// A와 B가 다른지 확인하고 다르면 i 값을 반환
-
-function swap(a, b) {
-  ;[A[a], A[b]] = [A[b], A[a]]
-  if (diff === a || diff === b) {
-    diff = different()
-    if (diff === false) answer = 1
-  }
-  // diff 값이 변화할 때마다 두 배열이 같은지 확인
-}
-
-function partition(p, r) {
-  const x = A[r]
-  let i = p - 1
-  for (let j = p; j <= r - 1; j++) {
-    if (A[j] <= x) swap(++i, j)
-  }
-  if (i + 1 !== r) swap(i + 1, r)
-  return i + 1
-}
-
-function quickSort(p, r) {
+const a = input[1].split(" ").map((v) => parseInt(v));
+const b = input[2].split(" ").map((v) => parseInt(v));
+let ans = 0;
+let diff = check(a);
+function quickSort(A, p, r) {
   if (p < r) {
-    const q = partition(p, r)
-    quickSort(p, q - 1)
-    quickSort(q + 1, r)
+    let q = partition(A, p, r);
+    if (!ans) quickSort(A, p, q - 1);
+    if (!ans) quickSort(A, q + 1, r);
   }
 }
 
-const N = Number(input[0])
-let A = input[1].split(' ').map(e => +e)
-const B = input[2].split(' ').map(e => +e)
-let answer = 0
-let diff = different()
-if (diff !== false) quickSort(0, N - 1)
-else answer = 1
-console.log(answer)
+function partition(A, p, r) {
+  let pivot = A[r];
+  let i = p - 1;
+
+  for (let j = p; j < r; j++) {
+    if (A[j] <= pivot) {
+      i++;
+      [A[i], A[j]] = [A[j], A[i]];
+      if (diff == i || diff == j) {
+        diff = check(a);
+      }
+    }
+  }
+  [A[i + 1], A[r]] = [A[r], A[i + 1]];
+  if (diff == r || diff == i + 1) {
+    diff = check(a);
+  }
+  return i + 1;
+}
+
+function check(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== b[i]) return i;
+  }
+  ans = 1;
+  return true;
+}
+
+if (ans) console.log(ans);
+else {
+  quickSort(a, 0, a.length - 1);
+  console.log(ans);
+}
