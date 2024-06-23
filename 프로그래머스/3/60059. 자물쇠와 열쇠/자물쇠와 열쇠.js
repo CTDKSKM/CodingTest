adae27b307ab3f2a5099fd956d1100a569650aec
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-23 22:24:57 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
@@ -1,23 +1,61 @@
-const pad = (size, v = 0) => Array(size).fill(v)
-function solution (key, lock) {
-    if (key.length < lock.length) {
-        const gap = lock.length - key.length
-        key = [...key.map(row => [...row, ...pad(gap)]), ...pad(gap, pad(lock.length))]
-    }
-    const m = key.length
-    for (let rN = 0; rN < 4; rN++) {
-        if (rN > 0) key = key[0].map((_, index) => key.map(row => row[index]).reverse())
-        for (let vN = -m + 1; vN <= m - 1; vN++) {
-            const vKey = key.map(row => vN >= 0
-                ? [...pad(vN), ...row.slice(0, m - vN)]
-                : [...row.slice(-m - vN), ...pad(Math.abs(vN))])
-            for (let hN = -m + 1; hN <= m - 1; hN++) {
-                const hKey = hN >= 0
-                    ? [...vKey.slice(-m + hN), ...pad(hN, pad(m))]
-                    : [...pad(Math.abs(hN), pad(m)), ...vKey.slice(0, m + hN)]
-                if (lock.every((row, i) => row.every((n, j) => (n + hKey[i][j]) === 1))) return true
+function solution(key, lock) {
+  let m = key.length,
+    n = lock.length;
+  const keys = getKeys(key, m);
+
+  let lockCnt = lock.reduce((acc, arr) => {
+    const cnt = arr.filter(v => !v).length;
+    return acc + cnt;
+  }, 0);
+  if (!lockCnt) return true;
+
+  // search
+  for (let x = -m; x < n; x++) {
+    for (let y = -m; y < n; y++) {
+      for (let k = 0; k < 4; k++) {
+        let matchCnt = 0,
+          wrong = 0;
+        for (let i = 0; i < m; i++) {
+          for (let j = 0; j < m; j++) {
+            // 범위 체크
+            if (x + i < 0 || y + j < 0 || x + i >= n || y + j >= n) continue;
+
+            // 일치 키 돌기 자물쇠 홈
+            if (keys[k][i][j] == 1 && lock[x + i][y + j] == 0) {
+              matchCnt++;
             }
+            // 어긋남 키 돌기, 자물쇠 돌기
+            else if (keys[k][i][j] == 1 && lock[x + i][y + j] == 1) {
+              wrong++;
+              break;
+            }
+          }
+          if (wrong) break;
         }
+        if (matchCnt == lockCnt && !wrong) {
+          return true;
+        }
+      }
     }
-    return false
+  }
+
+  return false;
 }
+
+const getKeys = (key, m) => {
+  const keys = [[], [], [], []];
+  for (let n = 0; n < 4; n++) {
+    for (let i = 0; i < m; i++) {
+      keys[n].push([]);
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < m; j++) {
+      keys[0][i][j] = key[i][j];
+      keys[1][i][j] = key[m - j - 1][i];
+      keys[2][i][j] = key[m - i - 1][m - j - 1];
+      keys[3][i][j] = key[j][m - i - 1];
+    }
+  }
+  return keys;
+};
