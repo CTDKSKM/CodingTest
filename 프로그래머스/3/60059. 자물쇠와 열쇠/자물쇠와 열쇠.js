8cecf8b8acc27b5d9462cd8b5570698f514e4578
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-23 22:23:20 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 1.00 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
@@ -1,83 +1,23 @@
-function solution(key, lock) {
-    const keyLen = key.length;
-    const lockLen = lock.length;
-
-    const mapLen = (keyLen)*2 + lockLen-2;
-    const mapGen = len => Array(len).fill(0).map(_=>Array(len).fill(0));
-    const maps = mapGen(mapLen);
-
-    // insert lock
-    for(let i=0;i<lockLen;i++){
-        for(let j=0;j<lockLen;j++){
-            const offset = keyLen - 1;
-            maps[offset+i][offset+j] = lock[i][j];
-        }
+const pad = (size, v = 0) => Array(size).fill(v)
+function solution (key, lock) {
+    if (key.length < lock.length) {
+        const gap = lock.length - key.length
+        key = [...key.map(row => [...row, ...pad(gap)]), ...pad(gap, pad(lock.length))]
     }
-
-    // only rectangle
-    // not clone
-    function rotate(arr) {
-        const arrLen = arr.length;
-        const arrLenHalf = Math.ceil(arrLen / 2);
-
-        for (let i = 0; i < arrLenHalf; i++) {
-            for (let j = i; j < arrLen - i - 1; j++) {
-                  const temp = arr[i][j];
-                  const offset = arrLen - i - 1;
-
-                  arr[i][j] = arr[offset - j + i][i];
-                  arr[offset - j + i][i] = arr[offset][offset - j + i];
-                  arr[offset][offset - j + i] = arr[j][offset];
-                  arr[j][offset] = temp;
+    const m = key.length
+    for (let rN = 0; rN < 4; rN++) {
+        if (rN > 0) key = key[0].map((_, index) => key.map(row => row[index]).reverse())
+        for (let vN = -m + 1; vN <= m - 1; vN++) {
+            const vKey = key.map(row => vN >= 0
+                ? [...pad(vN), ...row.slice(0, m - vN)]
+                : [...row.slice(-m - vN), ...pad(Math.abs(vN))])
+            for (let hN = -m + 1; hN <= m - 1; hN++) {
+                const hKey = hN >= 0
+                    ? [...vKey.slice(-m + hN), ...pad(hN, pad(m))]
+                    : [...pad(Math.abs(hN), pad(m)), ...vKey.slice(0, m + hN)]
+                if (lock.every((row, i) => row.every((n, j) => (n + hKey[i][j]) === 1))) return true
             }
         }
     }
-
-    function isAnswer(){
-        let res = true;
-        for(let i=0;i<lockLen;i++){
-            for(let j=0;j<lockLen;j++){
-                const offset = keyLen-1;
-                if(maps[offset+i][offset+j] % 2 == 0){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    function mapMark(offsetY, offsetX, mark){
-        for(let y=0; y<keyLen ;y++){
-            for(let x=0; x<keyLen ;x++){
-                maps[y + offsetY][x + offsetX] += key[y][x]*mark;
-            }
-        }
-    }
-
-    function unLock(){
-        const canMoveSize = mapLen - keyLen + 1;
-        for(let i=0;i < canMoveSize ;i++){
-            for(let j=0;j < canMoveSize ;j++){
-                const offsetY = i;
-                const offsetX = j;
-
-                mapMark(offsetY,offsetX, 1); 
-                if(isAnswer()) return true;
-                mapMark(offsetY,offsetX, -1); 
-            }
-        }
-        return false;
-    }
-
-    if(unLock()) return true;
-    rotate(key);
-
-    if(unLock()) return true;
-    rotate(key);
-
-    if(unLock()) return true;
-    rotate(key);
-
-    if(unLock()) return true;
-    return false;
+    return false
 }
