05763e457ce6f45c8a97266c7feef272ff41d9ac
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-06-23 22:22:21 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.71 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.js
@@ -1,59 +1,83 @@
 function solution(key, lock) {
-    const N = lock.length;
-    const M = key.length;
-    const extendedSize = N + 2 * (M - 1);
+    const keyLen = key.length;
+    const lockLen = lock.length;
 
-    const keys = [key];
-    for (let i = 1; i < 4; i++) {
-        keys.push(rotate(keys[i - 1]));
-    }
+    const mapLen = (keyLen)*2 + lockLen-2;
+    const mapGen = len => Array(len).fill(0).map(_=>Array(len).fill(0));
+    const maps = mapGen(mapLen);
 
-    const extendedLock = Array.from({ length: extendedSize }, () =>
-        Array(extendedSize).fill(0)
-    );
-
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; j++) {
-            extendedLock[i + M - 1][j + M - 1] = lock[i][j];
+    // insert lock
+    for(let i=0;i<lockLen;i++){
+        for(let j=0;j<lockLen;j++){
+            const offset = keyLen - 1;
+            maps[offset+i][offset+j] = lock[i][j];
         }
     }
 
-    for (const rotatedKey of keys) {
-        for (let x = 0; x <= extendedSize - M; x++) {
-            for (let y = 0; y <= extendedSize - M; y++) {
-                if (canUnlock(extendedLock, rotatedKey, x, y, N, M)) {
-                    return true;
+    // only rectangle
+    // not clone
+    function rotate(arr) {
+        const arrLen = arr.length;
+        const arrLenHalf = Math.ceil(arrLen / 2);
+
+        for (let i = 0; i < arrLenHalf; i++) {
+            for (let j = i; j < arrLen - i - 1; j++) {
+                  const temp = arr[i][j];
+                  const offset = arrLen - i - 1;
+
+                  arr[i][j] = arr[offset - j + i][i];
+                  arr[offset - j + i][i] = arr[offset][offset - j + i];
+                  arr[offset][offset - j + i] = arr[j][offset];
+                  arr[j][offset] = temp;
+            }
+        }
+    }
+
+    function isAnswer(){
+        let res = true;
+        for(let i=0;i<lockLen;i++){
+            for(let j=0;j<lockLen;j++){
+                const offset = keyLen-1;
+                if(maps[offset+i][offset+j] % 2 == 0){
+                    return false;
                 }
             }
         }
+        return true;
     }
-    return false;
-}
 
-function rotate(matrix) {
-    const N = matrix.length;
-    const rotated = Array.from({ length: N }, () => Array(N).fill(0));
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; j++) {
-            rotated[j][N - 1 - i] = matrix[i][j];
-        }
-    }
-    return rotated;
-}
-
-function canUnlock(extendedLock, key, startX, startY, lockSize, keySize) {
-    const lock = JSON.parse(JSON.stringify(extendedLock));
-    for (let i = 0; i < keySize; i++) {
-        for (let j = 0; j < keySize; j++) {
-            lock[startX + i][startY + j] += key[i][j];
-        }
-    }
-    for (let i = 0; i < lockSize; i++) {
-        for (let j = 0; j < lockSize; j++) {
-            if (lock[i + keySize - 1][j + keySize - 1] !== 1) {
-                return false;
+    function mapMark(offsetY, offsetX, mark){
+        for(let y=0; y<keyLen ;y++){
+            for(let x=0; x<keyLen ;x++){
+                maps[y + offsetY][x + offsetX] += key[y][x]*mark;
             }
         }
     }
-    return true;
+
+    function unLock(){
+        const canMoveSize = mapLen - keyLen + 1;
+        for(let i=0;i < canMoveSize ;i++){
+            for(let j=0;j < canMoveSize ;j++){
+                const offsetY = i;
+                const offsetX = j;
+
+                mapMark(offsetY,offsetX, 1); 
+                if(isAnswer()) return true;
+                mapMark(offsetY,offsetX, -1); 
+            }
+        }
+        return false;
+    }
+
+    if(unLock()) return true;
+    rotate(key);
+
+    if(unLock()) return true;
+    rotate(key);
+
+    if(unLock()) return true;
+    rotate(key);
+
+    if(unLock()) return true;
+    return false;
 }
