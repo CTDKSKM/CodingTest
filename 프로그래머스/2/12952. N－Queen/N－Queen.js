aef2c0d2e3177c9084cfa3e41f31be3934686e2f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-13 12:11:58 +0900

    [level 2] Title: N-Queen, Time: 423.63 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,46 +1,39 @@
 function solution(n) {
-  let count = 0;
-
-  function isSafe(board, row, col) {
-    for (let i = 0; i < row; i++) {
-      if (board[i][col] === 1) {
-        return false;
-      }
+    let answer = 0;
+    const board = new Array(n).fill().map(()=>new Array(n).fill(0))
+    const fill = (arr, y, x) => {
+        let cnt = 1;
+        while (y+cnt < n) {
+            const ny = y+cnt;
+            const r = x+cnt;
+            const l = x-cnt;
+            //아래
+            if (arr[ny][x] == 0) arr[ny][x] = 2;
+            //대각
+            if (r < n && arr[ny][r] == 0) arr[ny][r] = 2;
+            if (l >= 0 && arr[ny][l] == 0) arr[ny][l] = 2;
+            
+            cnt++
+        }
     }
-
-    for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-      if (board[i][j] === 1) {
-        return false;
-      }
+    
+    const set = (arr, y, x, num) => {
+        if (y === n-1) {
+            answer++
+            return
+        }
+        arr[y][x] = 1;
+        fill(arr, y, x);
+        
+        for(let i=0; i<n; i++) {
+            if (arr[y+1][i] !== 0) continue
+            set(arr.slice().map((_,idx)=>arr[idx].slice()), y+1, i, num+1);
+        }
     }
-
-    for (let i = row, j = col; i >= 0 && j < n; i--, j++) {
-      if (board[i][j] === 1) {
-        return false;
-      }
+    
+    for(let i=0; i<n; i++) {
+        set(board.slice().map((_,i)=>board[i].slice()), 0, i, 0);
     }
-
-    return true;
-  }
-
-  function solveNQueens(board, row) {
-    if (row === n) {
-      count++;
-      return;
-    }
-
-    for (let col = 0; col < n; col++) {
-      if (isSafe(board, row, col)) {
-        board[row][col] = 1;
-        solveNQueens(board, row + 1);
-        board[row][col] = 0;
-      }
-    }
-  }
-
-  const board = new Array(n).fill().map(() => new Array(n).fill(0));
-
-  solveNQueens(board, 0);
-
-  return count;
+    
+    return answer;
 }
