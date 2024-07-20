bdbc347dec11012be13d13adba36eef3b5218092
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-20 16:32:13 +0900

    [level 3] Title: 인사고과, Time: 97.86 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/README.md
+++ 프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995#) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 83.11 ms
+메모리: 56.6 MB, 시간: 97.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 20일 16:31:34
+2024년 07월 20일 16:32:10
 
 ### 문제 설명
 
--- 프로그래머스/3/152995. 인사고과/인사고과.js
+++ 프로그래머스/3/152995. 인사고과/인사고과.js
@@ -1,38 +1,16 @@
 function solution(scores) {
-    const pScore = scores[0].reduce((a,c)=>a+c,0);
-    
-    
-    const dict = {};
-    let aMax = 0;
-    let bMax = 0;
-    scores.forEach(([a,b])=>{
-        if (dict[a]) {
-            if (dict[a] < b) dict[a] = b
-        }
-        else dict[a] = b;
-        
-        aMax = Math.max(aMax, a);
-        bMax = Math.max(bMax, b);
-    })
-    const arr = Object.entries(dict).map(([a,b])=>([+a,b])).sort((a,b)=>a[0]-b[0]);
-
-    const filtered = [];
-    for(let i=0; i<scores.length; i++) {
-        const [a, b] = scores[i]
-        let cnt = arr.length-1;
-        let isPassed = true;
-        while (a < aMax && b < bMax && a < arr[cnt][0] && cnt && isPassed) {
-            if (b < arr[cnt][1]) {
-                isPassed = false;
-                break
-            }
-            cnt--
-        }
-        
-        if (i == 0 && !isPassed) return -1
-        
-        if (isPassed) filtered.push(a+b)
+    let answer = 0;
+  const wanho = scores[0];
+  scores = scores.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
+  let max = 0;
+  for(let i = 0 ; i < scores.length; i++) {
+    const score = scores[i];
+    if (score[1] < max) {
+      if (score[0] === wanho[0] && score[1] === wanho[1]) return -1;
+    } else {
+      max = Math.max(max, score[1]);
+      if (score[0] + score[1] > wanho[0] + wanho[1]) answer++;
     }
-    
-    return filtered.sort((a,b)=>b-a).findIndex((v)=>v == pScore) + 1
+  }
+  return answer + 1;
 }
