3f96b66af2a8970753abca2b6bf9897bee7f652f
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-20 22:07:30 +0900

    [level 3] Title: 양과 늑대, Time: 22.14 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -0,0 +1,38 @@
+function solution(info, edges) {
+    let answer = 0;
+    const n = info.length;
+    const tree = {};
+    const alps = Array.from(Array(n), (_, i) => String.fromCharCode(97 + i));
+    
+    const convertNumToAlp = (num) => alps[num]
+    const convertAlpToNum = (alp) => alps.indexOf(alp)
+    
+    edges = edges.map(([a,b])=>([convertNumToAlp(a),convertNumToAlp(b)]))
+    
+    edges.forEach((edge) => {
+        const [p, c] = edge;
+        if (!tree[c]) tree[c] = [];
+        if (!tree[p]) tree[p] = [];
+        tree[p].push(c);
+    })
+    
+    const possibles = [...tree['a']]
+    const findComb = (sheep, wolves, str, possible) => {
+        if (sheep <= wolves) return;
+        
+        answer = Math.max(answer, sheep);
+        
+        for(let i=0; i<n; i++) {
+            const nextStr = convertNumToAlp(i);
+            
+            if (str.includes(nextStr) || !possible.includes(nextStr)) continue
+            
+            info[i] 
+                ? findComb(sheep, wolves+1, str+nextStr, possible.slice().concat(...tree[nextStr])) 
+                : findComb(sheep+1, wolves, str+nextStr, possible.slice().concat(...tree[nextStr]))
+        }
+    }
+    findComb(1, 0, 'a', possibles);
+    
+    return answer;
+}
