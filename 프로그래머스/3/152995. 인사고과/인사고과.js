bc96610a8e2405b98ef58257038fe655df75c5af
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-20 17:03:06 +0900

    [level 3] Title: 인사고과, Time: 140.69 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.js
+++ 프로그래머스/3/152995. 인사고과/인사고과.js
@@ -1,16 +1,50 @@
 function solution(scores) {
-    let answer = 0;
+  const n = scores.length;
   const wanho = scores[0];
-  scores = scores.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
-  let max = 0;
-  for(let i = 0 ; i < scores.length; i++) {
-    const score = scores[i];
-    if (score[1] < max) {
-      if (score[0] === wanho[0] && score[1] === wanho[1]) return -1;
-    } else {
-      max = Math.max(max, score[1]);
-      if (score[0] + score[1] > wanho[0] + wanho[1]) answer++;
+
+  const peerMaxTable = scores.reduce((prev, [work, peer]) => {
+    if (!prev[work]) prev[work] = peer;
+    else prev[work] = Math.max(prev[work], peer);
+    return prev;
+  }, {});
+
+  const peerMinTable = {};
+  let prevMax = -1;
+  Object.keys(peerMaxTable)
+    .sort((a, b) => +b - +a)
+    .forEach(work => {
+      if (peerMaxTable[work] > prevMax) {
+        peerMinTable[work] = prevMax;
+        prevMax = peerMaxTable[work];
+      } else {
+        peerMinTable[work] = prevMax;
+      }
+    });
+
+  const bonusPeer = [];
+  for (let i = 0; i < n; i++) {
+    if (scores[i][1] >= peerMinTable[scores[i][0]]) {
+      bonusPeer.push(scores[i]);
     }
   }
-  return answer + 1;
+
+  const rankPeer = bonusPeer
+    .map(([work, peer]) => [work + peer, work, peer])
+    .sort((a, b) => b[0] - a[0]);
+
+  let rank = 0;
+  let count = 0;
+  let prev = -1;
+  for (let i = 0; i < rankPeer.length; i++) {
+    count++;
+    if (prev !== rankPeer[i][0]) {
+      rank = count;
+      prev = rankPeer[i][0];
+    }
+    if (rankPeer[i][1] === wanho[0] && rankPeer[i][2] === wanho[1]) {
+      return rank;
+    }
+  }
+
+  return -1;
 }
