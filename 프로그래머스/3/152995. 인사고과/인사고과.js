86399ee8a869ddd9174d74976c0fda12f3bfcd23
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-20 17:11:13 +0900

    [level 3] Title: 인사고과, Time: 68.37 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.js
+++ 프로그래머스/3/152995. 인사고과/인사고과.js
@@ -1,29 +1,22 @@
 function solution(scores) {
+    const wanho = scores[0];
+    
+    scores.sort((a,b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
+    
     let answer = 1;
-    let scoreIndex = {};
-    let my_score = scores[0][0] + scores[0][1];
+    let maxScore = 0;
+    const wanhoSum = wanho[0] + wanho[1];
 
-    const addScoreIndex = (first, second) => {
-        if(!scoreIndex[first])  scoreIndex[first] = [];
-        scoreIndex[first].push(second);
-    }
-
-    for(let score of scores)    addScoreIndex( ... score );
-    let keys = Object.keys(scoreIndex);
-
-    keys.sort((a, b) => b - a);
-    let max = 0;
-
-    for(let key of keys){
-        key = Number(key);
-        let new_max = max;
-        if(key === scores[0][0] && max > scores[0][1])  return -1;
-        for(let i = 0; i < scoreIndex[key].length; i++){
-            if(scoreIndex[key][i] > new_max)    new_max = scoreIndex[key][i];
-            else if(scoreIndex[key][i] < max)    continue;
-            if(scoreIndex[key][i] + key > my_score) answer++;
+    for(let score of scores){
+        if(score[1] < maxScore) {
+            if(score === wanho) return -1;
+        } else {
+            maxScore = Math.max(maxScore, score[1]);
+            if(score[0] + score[1] > wanhoSum){
+                answer++;
+            }
         }
-        max = new_max;
     }
+    
     return answer;
 }
