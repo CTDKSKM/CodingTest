923174f0630d0cd46644e391bec01a65dfd5944c
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-07-22 18:58:56 +0900

    [level 2] Title: 양궁대회, Time: 10.65 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,64 +1,54 @@
 function solution(n, info) {
-    let max = 0;
-    let answer = [-1];
-    let lion = Array(11).fill(0);
-
-    function DFS(level,count){
-        // 종료조건
-        if(level == 10){
-            lion[level] = count;
-            // 점수비교
-            let sum = 0
-            for (let i = 0; i < 10; i++) {
-                if(lion[i] > info[i]){
-                    sum = sum + (10 - i);
-                }else if(lion[i] === info[i]){
-                    continue;
-                }else{
-                    sum = sum - (10 - i);
-                }      
+    info.reverse();
+    let ans = [-1];
+    let maxd = 0;
+    
+    const combinations = [];
+    const generateCombinations = (current, index) => {
+        if (index === 11) {
+            combinations.push([...current]);
+            return;
+        }
+        current.push(true);
+        generateCombinations(current, index + 1);
+        current.pop();
+        current.push(false);
+        generateCombinations(current, index + 1);
+        current.pop();
+    };
+    generateCombinations([], 0);
+    
+    for (let wl of combinations) {
+        let t = 0;
+        let s = 0;
+        for (let i = 0; i < 11; i++) {
+            if (wl[i]) {
+                s += info[i] + 1;
             }
-
-            if(sum > max){
-                max = sum;
-                answer = [...lion];
-            }else if(sum == max){
-                // 낮은 개수를 더 맞추는 경우를 답으로 채용함
-                for (let j = 10; j > 0; j--) {
-                    if(answer[j] == lion[j]){
-                        continue;
-                    }else if(lion[j] > answer[j]){
-                        answer = [...lion];
-                        break;
-                    }else{
-                        break;
-                    }
+        }
+        if (s <= n) {
+            let apeach = 0;
+            let ryan = 0;
+            for (let i = 0; i < 11; i++) {
+                if (!wl[i] && info[i] > 0) {
+                    apeach += i;
+                }
+                if (wl[i]) {
+                    ryan += i;
                 }
             }
-        // 계속진행
-        }else{
-
-            // 남은 화살개수가 없거나 + 어피차보다 많이 못맞출경우
-            if(count == 0 || count < info[level] + 1 ){
-                DFS(level+1,count);
-            }else{
-                // 어피치보다 많이 맞출경우
-                lion[level] = info[level] + 1
-                count = count - (info[level] + 1);
-                DFS(level+1,count)
-
-
-                // 다른 점수로 돌릴경우
-                lion[level] = 0
-                count = count + (info[level] + 1);
-                DFS(level+1,count)
+            let d = ryan - apeach;
+            if (d > maxd) {
+                maxd = d;
+                ans = [];
+                for (let i = 0; i < 11; i++) {
+                    ans.push(wl[i] ? info[i] + 1 : 0);
+                }
+                ans[0] += n - s;
             }
-
         }
-
-
     }
-    DFS(0,n)
-
-    return answer;
+    
+    ans.reverse();
+    return ans;
 }
