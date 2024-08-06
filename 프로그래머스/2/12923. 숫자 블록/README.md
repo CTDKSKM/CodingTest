ac6ac8f86bac121c05c1a2308c2e7e26ce78bba0
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-08-06 20:30:15 +0900

    [level 2] Title: 숫자 블록, Time: 650.85 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/README.md
+++ 프로그래머스/2/12923. 숫자 블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 블록 - 12923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 1840.28 ms
+메모리: 37.2 MB, 시간: 650.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 20:25:43
+2024년 08월 06일 20:30:11
 
 ### 문제 설명
 
--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
@@ -4,18 +4,18 @@ function solution(begin, end) {
     const get = (num) => {
         if (num === 1) return 0
         
-        const temp = [];
+        let temp = null;
         
         for (let i = 2; i <= Math.sqrt(num); i++) {
             if (num % i === 0) {  // num이 i로 나누어 떨어진다면 (즉, i가 약수라면)
                 if (num / i <= 10000000) {
                     return num / i
                 }
-                else temp.push(i)
+                else temp = i
             }
         }
         
-        if (temp.length) return temp.at(-1);
+        if (temp) return temp
         
         return 1;
     }
