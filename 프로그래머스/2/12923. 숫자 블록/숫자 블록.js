52fd4bc5ffb9bbb20ab625b9415de21a99a14b88
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-08-06 20:14:50 +0900

    [level 2] Title: 숫자 블록, Time: 152.43 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
@@ -1,19 +1,8 @@
 function solution(begin, end) {
     const answer = [];
     
-    const isPrime = (num) => {
-        if (num <= 1) return false;
-        if (num === 2) return true;
-        const sqrt = Math.sqrt(num);
-        for (let i = 2; i <= sqrt; i++) {
-            if (num % i === 0) return false;
-        }
-        return true;
-    };
-    
     const get = (num) => {
         if (num === 1) return 0
-        if (isPrime(num)) return 1
         
         const temp = [];
         
@@ -26,7 +15,9 @@ function solution(begin, end) {
             }
         }
         
-        return temp.at(-1);
+        if (temp.length) return temp.at(-1);
+        
+        return 1;
     }
 
     for (let i = begin; i <= end; i++) {
