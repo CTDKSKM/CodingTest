83e1edc57c7bf5ba1b1c50a07fd72edd303d3e30
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-08-17 18:17:27 +0900

    [level 2] Title: 순위 검색, Time: 583.62 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.js
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.js
@@ -1,70 +1,23 @@
-function generateCombinations(arrays) {
-    let results = [''];
-
-    for (let i = 0; i < arrays.length; i++) {
-        let tempResults = [];
-        for (let j = 0; j < results.length; j++) {
-            for (let k = 0; k < arrays[i].length; k++) {
-                tempResults.push(results[j] + arrays[i][k]);
-            }
-        }
-        results = tempResults;
+function bisect_gt(a, x) {
+    let lo = 0, hi = a.length;
+    while (lo < hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        if (a[mid] < x) lo = mid + 1;
+        else hi = mid;
     }
-
-    return results;
+    return a.length - lo;
 }
+
 function solution(info, query) {
-    const dict = {};
-    
-    info.forEach(entry => {
-        const [lang, work, exp, soulfood, score] = entry.split(' ');
-        const scoreValue = parseInt(score, 10);
-        const combinations = generateCombinations([
-            [lang, '-'],
-            [work, '-'],
-            [exp, '-'],
-            [soulfood, '-']
-        ]);
-        
-        combinations.forEach(comb => {
-            if (!dict[comb]) dict[comb] = [];
-            dict[comb].push(scoreValue);
-        });
-    });
-    
-    for (let key in dict) {
-        dict[key].sort((a, b) => a - b);
+    const table = {"c": 3, "j": 5, "p": 6, "b": 6, "f": 5, "s": 6, "-": 0};
+    const conv = (l, t) => [l.slice(0, -1).reduce((a, k) => (a << 3) + t(table[k[0]]), 0), Number(l[l.length - 1])];
+    info = info.map(s => conv(s.split(" "), x => 7 - x));
+    query = query.map(s => conv(s.split(" ").filter(c => c != "and"), x => x));
+    const map = new Map();
+    for (const [k, v] of info) {
+        if (!map.has(k)) map.set(k, []);
+        map.get(k).push(v);
     }
-    
-    const ans = [];
-    query.forEach(q=>{
-        const [lang, work, exp, rest] = q.split(' and ');
-        const [soulfood, minScore] = rest.split(' ');
-        const searchKey = lang + work + exp + soulfood;
-        const scores = dict[searchKey] || [];
-
-        let left = 0;
-        let right = scores.length;
-
-        while (left < right) {
-            const mid = Math.floor((left + right) / 2);
-            if (scores[mid] >= minScore) right = mid;
-            else left = mid + 1;
-        }
-
-        ans.push(scores.length - left);
-    })
-
-    return ans;
+    const dict = Array.from(map.entries()).map(([k, l]) => [k, l.sort((a, b) => a - b)])
+    return query.map(([q, v]) => dict.reduce((a, [k, l]) => a + (k & q ? 0 : bisect_gt(l, v)), 0));
 }
-/*
-[개발언어, 직군, 경력, 소울푸드, 점수] = info.split(' ')  1 이상 50,000 이하
-개발언어 = cpp | java | python
-직군 = backend | frontend
-경력 = junior | senior
-소울푸드 = chicken | pizza
-점수 = 1 이상 100,000 이하
-
-[개발언어, 직군, 경력, 소울푸드, 점수] = query.split('and')  1 이상 100,000 이하
-'-' 표시는 해당 조건을 고려하지 않겠다는 의미
-*/
