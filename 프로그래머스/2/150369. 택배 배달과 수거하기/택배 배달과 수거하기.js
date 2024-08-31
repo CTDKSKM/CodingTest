33663ff59b8845d8262c47fa28ba004fde2e22c4
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-09-01 00:02:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 51.75 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,32 +1,28 @@
 function solution(cap, n, ds, ps) {
-    let ans = 0;
-    let dIdx = n-1;
-    let pIdx = n-1;
+    let ans = 0, dIdx = n-1, pIdx = n-1;
+    
     for (let i = n - 1; i >= 0; i--) {
         while (ds[i] > 0 || ps[i] > 0) {
             ans += (i + 1) * 2; // 왕복 거리 계산
-
-            let dNum = cap; // 배달 가능한 용량
-            let pNum = 0;   // 수거한 용량
             
-            //배달
+            let dNum = cap, pNum = 0;
+            
             for (dIdx; dIdx >= 0; dIdx--) {
                 if (ds[dIdx] > 0) {
                     let dAmount = Math.min(dNum, ds[dIdx]);
                     dNum -= dAmount;
                     ds[dIdx] -= dAmount;
                 }
-                if (dNum === 0) break;
+                if (dNum === 0) break
             }
             
-            //수거
-            for (pIdx; pIdx>=0; pIdx--) {
+            for (pIdx; pIdx >= 0; pIdx--) {
                 if (ps[pIdx] > 0) {
                     let pAmount = Math.min(cap - pNum, ps[pIdx]);
                     pNum += pAmount;
                     ps[pIdx] -= pAmount;
                 }
-                if (pNum === cap) break;
+                if (pNum === cap) break
             }
         }
     }
