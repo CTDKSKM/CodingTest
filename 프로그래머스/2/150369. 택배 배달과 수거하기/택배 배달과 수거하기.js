dc14f43d45d6f5adae024ef9af9771dfcaaa26c8
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-09-01 00:02:50 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 41.85 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,31 +1,31 @@
-function solution(cap, n, ds, ps) {
-    let ans = 0, dIdx = n-1, pIdx = n-1;
-    
-    for (let i = n - 1; i >= 0; i--) {
-        while (ds[i] > 0 || ps[i] > 0) {
-            ans += (i + 1) * 2; // 왕복 거리 계산
-            
-            let dNum = cap, pNum = 0;
-            
-            for (dIdx; dIdx >= 0; dIdx--) {
-                if (ds[dIdx] > 0) {
-                    let dAmount = Math.min(dNum, ds[dIdx]);
-                    dNum -= dAmount;
-                    ds[dIdx] -= dAmount;
-                }
-                if (dNum === 0) break
-            }
-            
-            for (pIdx; pIdx >= 0; pIdx--) {
-                if (ps[pIdx] > 0) {
-                    let pAmount = Math.min(cap - pNum, ps[pIdx]);
-                    pNum += pAmount;
-                    ps[pIdx] -= pAmount;
-                }
-                if (pNum === cap) break
-            }
-        }
+function solution(cap, n, deliveries, pickups) {
+  let answer = 0;
+  let [i, j] = [n - 1, n - 1];
+
+  const pickOrDeliver = (idx, iter) => {
+    let box = 0;
+    while (idx >= 0 && box <= cap) {
+      if (box + iter[idx] <= cap) {
+        box += iter[idx];
+        idx--;
+      } else {
+        iter[idx] -= cap - box;
+        break;
+      }
+    }
+    return idx;
+  };
+
+  while (i >= 0 || j >= 0) {
+    if (deliveries[i] === 0 && pickups[j] === 0) {
+      i--;
+      j--;
+      continue;
     }
 
-    return ans;
+    answer += (Math.max(i, j) + 1) * 2;
+    i = pickOrDeliver(i, deliveries);
+    j = pickOrDeliver(j, pickups);
+  }
+  return answer;
 }
