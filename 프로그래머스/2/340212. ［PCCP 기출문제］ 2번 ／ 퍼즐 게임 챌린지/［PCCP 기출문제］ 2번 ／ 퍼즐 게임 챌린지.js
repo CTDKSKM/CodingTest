0d3252a6c352f27d8c1b2ab129fad6f6841afad6
CTDKSKM <128657814+CTDKSKM@users.noreply.github.com>
2024-09-22 13:44:12 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 39.61 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,49 +1,16 @@
 function solution(diffs, times, limit) {
-    let low = diffs[0];
-    let high = diffs[0];
-    diffs.forEach((needLev)=>{
-        if (needLev < low) low = needLev;
-        if (needLev > high) high = needLev;
-    })
-
+    let low = 1;
+    let high = 100_000;
     let min = high;
-    
-    const calc = (level) => {
-        let remain = limit;
-        let prevTime = 0;
-        
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2), remain = limit, prev = 0;
         for (let i = 0; i < diffs.length; i++) {
-            const attempts = diffs[i] - level;
-            const curTime = times[i];
-
-            if (attempts > 0) {
-                remain -= (curTime + prevTime) * attempts + curTime;
-            } else {
-                remain -= curTime;
-            }
-
-            if (remain < 0) {
-                break;
-            }
-
-            prevTime = curTime;
+            remain -= diffs[i] > mid ? (times[i] + prev) * (diffs[i] - mid) + times[i] : times[i];
+            if (remain < 0) break;
+            prev = times[i];
         }
-        
-        return remain;
-    }
-    
-    while ( low <= high ) {
-        let mid = Math.floor( ( low + high ) / 2 );
-        
-        let remain = calc(mid);
-        
-        if ( remain >= 0 ) {
-            min = mid;
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
+        remain >= 0 ? (min = mid, high = mid - 1) : low = mid + 1;
     }
     
     return min;
